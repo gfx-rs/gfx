ef69543a78ceb94486556e610f47d29e5149452b
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2014-06-18 10:49:07 -0700

    Add rough example of gfx::start

--- src/gfx/lib.rs
+++ src/gfx/lib.rs
@@ -12,6 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! ~~~rust
+//! extern crate gfx;
+//!
+//! #[start]
+//! fn start(argc: int, argv: **u8) -> int {
+//!     native::start(argc, argv, main)
+//! }
+//!
+//! fn main() {
+//!     // spawn render task
+//!     let (renderer, platform) = gfx::start(()).unwrap();
+//!
+//!     // spawn game task
+//!     spawn(proc {
+//!         let _ = renderer; // do stuff with renderer
+//!     })
+//!
+//!     loop {
+//!         platform.update(); // update platform
+//!     }
+//! }
+//! ~~~
+//!
 //! ~~~
 //!     Render Task        |           Main Platform Thread             |         User Task
 //!                        |                                            |
