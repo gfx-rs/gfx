b904023e2fc18c75a0b22726295a35a4a28de392
Dzmitry Malyshau <kvarkus@gmail.com>
2014-07-23 23:50:39 -0400

    Mesh macro - Partially implemented type decoding, improved general structure.

--- src/macro/mesh.rs
+++ src/macro/mesh.rs
@@ -20,9 +20,54 @@ use syntax::ext::deriving::generic;
 use syntax::codemap;
 use syntax::parse::token;
 
+fn decode_type(cx: &mut ext::base::ExtCtxt, span: codemap::Span, ty: &str) -> Gc<ast::Expr> {
+    cx.expr_lit(span, ast::LitIntUnsuffixed(0))
+}
 
-fn decode_type(cx: &mut ext::base::ExtCtxt, span: codemap::Span) -> (Gc<ast::Expr>, Gc<ast::Expr>) {
-    (cx.expr_uint(span, 0), cx.expr_uint(span, 0))
+fn decode_count_type(cx: &mut ext::base::ExtCtxt, span: codemap::Span, ty: &ast::Ty_) -> (Gc<ast::Expr>, Gc<ast::Expr>) {
+    let (count, etype) = match *ty {
+        ast::TyPath(ref p, _, _) => (1, decode_type(cx, span, p.segments[0].identifier.name.as_str())),
+        ast::TyFixedLengthVec(pty, expr) => {
+            let t = match pty.node {
+                ast::TyPath(ref p, _, _) => decode_type(cx, span, p.segments[0].identifier.name.as_str()),
+                _ => {
+                    cx.span_err(span, format!(
+                        "Unsupported fixed vector sub-type: {}",
+                        pty.node).as_slice()
+                    );
+                    cx.expr_lit(span, ast::LitNil)
+                },
+            };
+            let c = match expr.node {
+                ast::ExprLit(plit) => match plit.node {
+                    ast::LitIntUnsuffixed(value) => value,
+                    _ => {
+                        cx.span_err(span, format!(
+                            "Unsupported vector size literal: {}",
+                            plit.node).as_slice()
+                        );
+                        0
+                    },
+                },
+                _ => {
+                    cx.span_err(span, format!(
+                        "Unsupported vector size: {}",
+                        expr.node).as_slice()
+                    );
+                    0
+                },
+            };
+            (c, t)
+        },
+        _ => {
+            cx.span_err(span, format!(
+                "Unsupported attribute type: {}",
+                ty).as_slice()
+            );
+            (0, cx.expr_lit(span, ast::LitNil))
+        },
+    };
+    (cx.expr_lit(span, ast::LitIntUnsuffixed(count)), etype)
 }
 
 /// `generate()` method generating code
@@ -32,23 +77,42 @@ fn method_generate(cx: &mut ext::base::ExtCtxt, span: codemap::Span,
         generic::StaticStruct(ref definition, generic::Named(ref fields)) => {
             let mut statements = Vec::new();
             let id_at = cx.ident_of("at");
-            statements.push(cx.stmt_let(
-                span,
-                true,
-                id_at,
-                cx.expr_call(span, cx.expr_vec_ng(span), Vec::new())
-                ));
-            let ex_stride = cx.expr_uint(span, 0);  //TODO
-            let mut offset = 0u;    //TODO
+            statements.push(cx.stmt_let(span, true, id_at, cx.expr_vec_ng(span)));
+            let ex_offset = cx.expr_lit(span, ast::LitIntUnsuffixed(0));    //TODO
+            let ex_stride = cx.expr_cast(span,
+                cx.expr_call(span,
+                    cx.expr_path(cx.path_all(
+                        span,
+                        true,
+                        vec![
+                            cx.ident_of("std"),
+                            cx.ident_of("mem"),
+                            cx.ident_of("size_of")
+                        ],
+                        Vec::new(),
+                        vec![cx.ty_ident(span, substr.type_ident)]
+                    )),
+                    Vec::new()
+                ),
+                cx.ty_path(cx.path_global(span, vec![
+                    cx.ident_of("gfx"),
+                    cx.ident_of("attrib"),
+                    cx.ident_of("Stride")
+                    ]), None)
+            );
             for (def, &(ident, _)) in definition.fields.iter().zip(fields.iter()) {
-                let (ex_count, ex_type) = decode_type(cx, span);
+                let (ex_count, ex_type) = decode_count_type(cx, span, &def.node.ty.node);
                 let ex_struct = cx.expr_struct(span,
-                    cx.path(span, vec![cx.ident_of("gfx"), cx.ident_of("Attribute")]),
+                    cx.path(span, vec![
+                        cx.ident_of("gfx"),
+                        cx.ident_of("mesh"),
+                        cx.ident_of("Attribute")
+                        ]),
                     vec![
                         cx.field_imm(span, cx.ident_of("buffer"), substr.nonself_args[1]),
                         cx.field_imm(span, cx.ident_of("elem_count"), ex_count),
                         cx.field_imm(span, cx.ident_of("elem_type"), ex_type),
-                        cx.field_imm(span, cx.ident_of("offset"), cx.expr_uint(span, offset)),
+                        cx.field_imm(span, cx.ident_of("offset"), ex_offset),
                         cx.field_imm(span, cx.ident_of("stride"), ex_stride),
                         cx.field_imm(span, cx.ident_of("name"), cx.expr_method_call(span,
                             cx.expr_str(span, token::get_ident(ident)),
@@ -62,7 +126,12 @@ fn method_generate(cx: &mut ext::base::ExtCtxt, span: codemap::Span,
                     vec![ex_struct]
                     )));
             }
-            cx.expr_block(cx.block_all(span, Vec::new(), statements, None))
+            cx.expr_block(cx.block_all(
+                span,
+                Vec::new(),
+                statements,
+                Some(cx.expr_ident(span, id_at))
+                ))
         },
         _ => {
             cx.span_err(span, "Unable to implement `generate()` on a non-structure");
@@ -80,7 +149,7 @@ pub fn expand_vertex_format(context: &mut ext::base::ExtCtxt, span: codemap::Spa
         span: span,
         attributes: Vec::new(),
         path: generic::ty::Path {
-            path: vec!["gfx", "VertexFormat"],
+            path: vec!["gfx", "mesh", "VertexFormat"],
             lifetime: None,
             params: Vec::new(),
             global: true,
@@ -109,7 +178,7 @@ pub fn expand_vertex_format(context: &mut ext::base::ExtCtxt, span: codemap::Spa
                         lifetime: None,
                         params: vec![
                             box generic::ty::Literal(generic::ty::Path::new(
-                                vec!["gfx", "Attribute"])),
+                                vec!["gfx", "mesh", "Attribute"])),
                         ],
                         global: false,
                     },
