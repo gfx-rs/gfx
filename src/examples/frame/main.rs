ebc17b9ee6db8554aef667e7fd9c8174fc9a8198
qres <qres@users.noreply.github.com>
2014-08-24 17:45:54 +0200

    updated frame example to match cube example

--- src/examples/frame/main.rs
+++ src/examples/frame/main.rs
@@ -21,47 +21,54 @@ use glfw::Context;
 #[vertex_format]
 struct Vertex {
     #[as_float]
-    a_Pos: [i8, ..3],
+    #[name = "a_Pos"]
+    pos: [i8, ..3],
+
     #[as_float]
-    a_TexCoord: [u8, ..2],
+    #[name = "a_TexCoord"]
+    tex_coord: [u8, ..2],
 }
 
-impl Vertex {
-    fn new(pos: [i8, ..3], tc: [u8, ..2]) -> Vertex {
-        Vertex {
-            a_Pos: pos,
-            a_TexCoord: tc,
-        }
-    }
-}
-
-#[shader_param(Program)]
+// The shader_param attribute makes sure the following struct can be used to
+// pass parameters to a shader. Its argument is the name of the type that will
+// be generated to represent your the program. Search for link_program below, to
+// see how it's used.
+#[shader_param(MyProgram)]
 struct Params {
-    u_ModelViewProj: [[f32, ..4], ..4],
-    t_Color: gfx::shade::TextureParam,
+    #[name = "u_Transform"]
+    transform: [[f32, ..4], ..4],
+
+    #[name = "t_Color"]
+    color: gfx::shade::TextureParam,
 }
 
 static VERTEX_SRC: gfx::ShaderSource = shaders! {
 GLSL_120: b"
     #version 120
+
     attribute vec3 a_Pos;
     attribute vec2 a_TexCoord;
     varying vec2 v_TexCoord;
-    uniform mat4 u_ModelViewProj;
+
+    uniform mat4 u_Transform;
+
     void main() {
         v_TexCoord = a_TexCoord;
-        gl_Position = u_ModelViewProj * vec4(a_Pos, 1.0);
+        gl_Position = u_Transform * vec4(a_Pos, 1.0);
     }
 "
 GLSL_150: b"
     #version 150 core
+
     in vec3 a_Pos;
     in vec2 a_TexCoord;
     out vec2 v_TexCoord;
-    uniform mat4 u_ModelViewProj;
+
+    uniform mat4 u_Transform;
+
     void main() {
         v_TexCoord = a_TexCoord;
-        gl_Position = u_ModelViewProj * vec4(a_Pos, 1.0);
+        gl_Position = u_Transform * vec4(a_Pos, 1.0);
     }
 "
 };
@@ -69,8 +76,10 @@ GLSL_150: b"
 static FRAGMENT_SRC: gfx::ShaderSource = shaders! {
 GLSL_120: b"
     #version 120
+
     varying vec2 v_TexCoord;
     uniform sampler2D t_Color;
+
     void main() {
         vec4 tex = texture2D(t_Color, v_TexCoord);
         float blend = dot(v_TexCoord-vec2(0.5,0.5), v_TexCoord-vec2(0.5,0.5));
@@ -79,8 +88,10 @@ GLSL_120: b"
 "
 GLSL_150: b"
     #version 150 core
+
     in vec2 v_TexCoord;
     out vec4 o_Color;
+
     uniform sampler2D t_Color;
     void main() {
         vec4 tex = texture(t_Color, v_TexCoord);
@@ -117,43 +128,43 @@ fn main() {
     let frame = gfx::Frame::new(w as u16, h as u16);
 
     let mut device = gfx::GlDevice::new(|s| glfw.get_proc_address(s));
-    let mut list = device.create_draw_list();
+    let mut renderer = device.create_renderer();
 
     let state = gfx::DrawState::new().depth(gfx::state::LessEqual, true);
 
     let (gbuffer, t_color) = create_gbuffer(w as u16, h as u16, &mut device);
 
     let vertex_data = vec![
-        //top (0, 0, 1)
-        Vertex::new([-1, -1,  1], [0, 0]),
-        Vertex::new([ 1, -1,  1], [1, 0]),
-        Vertex::new([ 1,  1,  1], [1, 1]),
-        Vertex::new([-1,  1,  1], [0, 1]),
-        //bottom (0, 0, -1)
-        Vertex::new([ 1,  1, -1], [0, 0]),
-        Vertex::new([-1,  1, -1], [1, 0]),
-        Vertex::new([-1, -1, -1], [1, 1]),
-        Vertex::new([ 1, -1, -1], [0, 1]),
-        //right (1, 0, 0)
-        Vertex::new([ 1, -1, -1], [0, 0]),
-        Vertex::new([ 1,  1, -1], [1, 0]),
-        Vertex::new([ 1,  1,  1], [1, 1]),
-        Vertex::new([ 1, -1,  1], [0, 1]),
-        //left (-1, 0, 0)
-        Vertex::new([-1,  1,  1], [0, 0]),
-        Vertex::new([-1, -1,  1], [1, 0]),
-        Vertex::new([-1, -1, -1], [1, 1]),
-        Vertex::new([-1,  1, -1], [0, 1]),
-        //front (0, 1, 0)
-        Vertex::new([-1,  1, -1], [0, 0]),
-        Vertex::new([ 1,  1, -1], [1, 0]),
-        Vertex::new([ 1,  1,  1], [1, 1]),
-        Vertex::new([-1,  1,  1], [0, 1]),
-        //back (0, -1, 0)
-        Vertex::new([ 1, -1,  1], [0, 0]),
-        Vertex::new([-1, -1,  1], [1, 0]),
-        Vertex::new([-1, -1, -1], [1, 1]),
-        Vertex::new([ 1, -1, -1], [0, 1]),
+        // top (0, 0, 1)
+        Vertex { pos: [-1, -1,  1], tex_coord: [0, 0] },
+        Vertex { pos: [ 1, -1,  1], tex_coord: [1, 0] },
+        Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
+        Vertex { pos: [-1,  1,  1], tex_coord: [0, 1] },
+        // bottom (0, 0, -1)
+        Vertex { pos: [ 1,  1, -1], tex_coord: [0, 0] },
+        Vertex { pos: [-1,  1, -1], tex_coord: [1, 0] },
+        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
+        Vertex { pos: [ 1, -1, -1], tex_coord: [0, 1] },
+        // right (1, 0, 0)
+        Vertex { pos: [ 1, -1, -1], tex_coord: [0, 0] },
+        Vertex { pos: [ 1,  1, -1], tex_coord: [1, 0] },
+        Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
+        Vertex { pos: [ 1, -1,  1], tex_coord: [0, 1] },
+        // left (-1, 0, 0)
+        Vertex { pos: [-1,  1,  1], tex_coord: [0, 0] },
+        Vertex { pos: [-1, -1,  1], tex_coord: [1, 0] },
+        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
+        Vertex { pos: [-1,  1, -1], tex_coord: [0, 1] },
+        // front (0, 1, 0)
+        Vertex { pos: [-1,  1, -1], tex_coord: [0, 0] },
+        Vertex { pos: [ 1,  1, -1], tex_coord: [1, 0] },
+        Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
+        Vertex { pos: [-1,  1,  1], tex_coord: [0, 1] },
+        // back (0, -1, 0)
+        Vertex { pos: [ 1, -1,  1], tex_coord: [0, 0] },
+        Vertex { pos: [-1, -1,  1], tex_coord: [1, 0] },
+        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
+        Vertex { pos: [ 1, -1, -1], tex_coord: [0, 1] },
     ];
 
     let mesh = device.create_mesh(vertex_data);
@@ -169,14 +180,14 @@ fn main() {
         ];
 
         let buf = device.create_buffer_static(&index_data);
-        gfx::IndexSlice8(buf, 0, 36)
+        gfx::IndexSlice8(gfx::TriangleList, buf, 0, 36)
     };
 
     let tinfo = gfx::tex::TextureInfo {
         width: 2,
         height: 2,
         depth: 1,
-        mipmap_range: (0, 1),
+        levels: 1,
         kind: gfx::tex::Texture2D,
         format: gfx::tex::RGBA8,
     };
@@ -192,8 +203,17 @@ fn main() {
     let sampler = device.create_sampler(gfx::tex::SamplerInfo::new(
         gfx::tex::Scale, gfx::tex::Clamp));
 
-    let prog: Program = device.link_program(VERTEX_SRC.clone(),
-        FRAGMENT_SRC.clone()).unwrap();
+    let prog: MyProgram = device
+        .link_program(VERTEX_SRC.clone(), FRAGMENT_SRC.clone())
+        .unwrap();
+
+    let view: AffineMatrix3<f32> = Transform::look_at(
+        &Point3::new(1.5f32, -5.0, 3.0),
+        &Point3::new(0f32, 0.0, 0.0),
+        &Vector3::unit_z(),
+    );
+    let aspect = w as f32 / h as f32;
+    let proj = cgmath::perspective(cgmath::deg(45.0f32), aspect, 1.0, 10.0);
 
     let mut m_model = Matrix4::<f32>::identity();
     let m_viewproj = {
@@ -209,12 +229,25 @@ fn main() {
     };
 
     let mut data = Params {
-        u_ModelViewProj: Matrix4::identity().into_fixed(),
-        t_Color: (t_color, Some(sampler)),
+        transform: proj.mul_m(&view.mat).into_fixed(),
+        color: (t_color, Some(sampler)),
     };
     let mut data_gbuffer_pass = Params {
-        u_ModelViewProj: Matrix4::identity().into_fixed(),
-        t_Color: (texture, Some(sampler)),
+        transform: proj.mul_m(&view.mat).into_fixed(),
+        color: (texture, Some(sampler)),
+    };
+
+
+    let clear_data = gfx::ClearData {
+        color: Some([0.3, 0.3, 0.3, 1.0]),
+        depth: Some(1.0),
+        stencil: None,
+    };
+
+    let clear_data_gbuffer = gfx::ClearData {
+        color: Some([0.3, 1.0, 0.3, 1.0]),
+        depth: Some(1.0),
+        stencil: None,
     };
 
     while !window.should_close() {
@@ -228,29 +261,15 @@ fn main() {
             }
         }
         // render
-        list.reset();
-        list.clear(
-            gfx::ClearData {
-                color: Some(gfx::Color([0.3, 0.3, 0.3, 1.0])),
-                depth: Some(1.0),
-                stencil: None,
-            },
-            &frame
-        );
-        list.clear(
-            gfx::ClearData {
-                color: Some(gfx::Color([0.3, 1.0, 0.3, 1.0])),
-                depth: Some(1.0),
-                stencil: None,
-            },
-            &gbuffer
-        );
+        renderer.reset();
+        renderer.clear(clear_data, &frame);
+        renderer.clear(clear_data_gbuffer, &gbuffer);
         m_model.x.x = 1.0;
-        data.u_ModelViewProj = m_viewproj.mul_m(&m_model).into_fixed();
-        data_gbuffer_pass.u_ModelViewProj = data.u_ModelViewProj;
-        list.draw(&mesh, slice, &gbuffer, (&prog, &data_gbuffer_pass), &state).unwrap();
-        list.draw(&mesh, slice, &frame, (&prog, &data), &state).unwrap();
-        device.submit(list.as_slice());
+        data.transform = m_viewproj.mul_m(&m_model).into_fixed();
+        data_gbuffer_pass.transform = data.transform;
+        renderer.draw(&mesh, slice, &gbuffer, (&prog, &data_gbuffer_pass), &state).unwrap();
+        renderer.draw(&mesh, slice, &frame, (&prog, &data), &state).unwrap();
+        device.submit(renderer.as_buffer());
         window.swap_buffers();
     }
 }
@@ -258,35 +277,35 @@ fn main() {
 
 
 fn create_gbuffer(width: u16, height: u16, renderer: &mut gfx::GlDevice) -> (gfx::Frame, TextureHandle) {
-  let mut f_gbuffer = gfx::Frame::new(width as u16, height as u16);
+    let mut f_gbuffer = gfx::Frame::new(width as u16, height as u16);
 
-  let tinfo_float = gfx::tex::TextureInfo {
-      width: width,
-      height: height,
-      depth: 1,
-      mipmap_range: (0, 1),
-      kind: gfx::tex::Texture2D,
-      format: gfx::tex::Float(gfx::tex::RGBA, gfx::attrib::F16),
-  };
-  let tinfo_depth = gfx::tex::TextureInfo {
-      width: width,
-      height: height,
-      depth: 1,
-      mipmap_range: (0, 1),
-      kind: gfx::tex::Texture2D,
-      format: gfx::tex::DEPTH24_STENCIL8,
-  };
-  let t_color = renderer.create_texture(tinfo_float).unwrap();
-  let t_normal = renderer.create_texture(tinfo_float).unwrap();
-  let t_depth = renderer.create_texture(tinfo_depth).unwrap();
+    let tinfo_float = gfx::tex::TextureInfo {
+        width: width,
+        height: height,
+        depth: 1,
+        levels: 1,
+        kind: gfx::tex::Texture2D,
+        format: gfx::tex::Float(gfx::tex::RGBA, gfx::attrib::F16),
+    };
+    let tinfo_depth = gfx::tex::TextureInfo {
+        width: width,
+        height: height,
+        depth: 1,
+        levels: 1,
+        kind: gfx::tex::Texture2D,
+        format: gfx::tex::DEPTH24STENCIL8,
+    };
+    let t_color = renderer.create_texture(tinfo_float).unwrap();
+    //let t_normal = renderer.create_texture(tinfo_float).unwrap();
+    let t_depth = renderer.create_texture(tinfo_depth).unwrap();
 
-  f_gbuffer.colors = [
-    PlaneTexture(t_color.get_name(), 0 as Level, None),
-    PlaneTexture(t_normal.get_name(), 0 as Level, None),
-    PlaneEmpty,
-    PlaneEmpty,
-  ];
-  f_gbuffer.depth   = PlaneTexture(t_depth.get_name(), 0, None);
-  //f_gbuffer.stencil = f_gbuffer.depth;
-  (f_gbuffer, t_color)
+    f_gbuffer.colors = [
+        PlaneTexture(t_color.get_name(), 0 as Level, None),
+        PlaneEmpty,
+        PlaneEmpty,
+        PlaneEmpty,
+    ];
+    f_gbuffer.depth   = PlaneTexture(t_depth.get_name(), 0, None);
+    //f_gbuffer.stencil = f_gbuffer.depth;
+    (f_gbuffer, t_color)
 }
