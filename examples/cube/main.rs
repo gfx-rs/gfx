96c1a9192021e2301086ab19bdcbf4249c2e2594
Jakob Fries <jakob.fries@gmail.com>
2014-11-17 21:46:26 +0100

    Corrected cube example: faces now have correct winding.

--- examples/cube/main.rs
+++ examples/cube/main.rs
@@ -147,25 +147,25 @@ fn main() {
         Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
         Vertex { pos: [-1,  1,  1], tex_coord: [0, 1] },
         // bottom (0, 0, -1)
-        Vertex { pos: [ 1,  1, -1], tex_coord: [0, 0] },
         Vertex { pos: [-1,  1, -1], tex_coord: [1, 0] },
-        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
+        Vertex { pos: [ 1,  1, -1], tex_coord: [0, 0] },
         Vertex { pos: [ 1, -1, -1], tex_coord: [0, 1] },
+        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
         // right (1, 0, 0)
         Vertex { pos: [ 1, -1, -1], tex_coord: [0, 0] },
         Vertex { pos: [ 1,  1, -1], tex_coord: [1, 0] },
         Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
         Vertex { pos: [ 1, -1,  1], tex_coord: [0, 1] },
         // left (-1, 0, 0)
-        Vertex { pos: [-1,  1,  1], tex_coord: [0, 0] },
         Vertex { pos: [-1, -1,  1], tex_coord: [1, 0] },
-        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
+        Vertex { pos: [-1,  1,  1], tex_coord: [0, 0] },
         Vertex { pos: [-1,  1, -1], tex_coord: [0, 1] },
+        Vertex { pos: [-1, -1, -1], tex_coord: [1, 1] },
         // front (0, 1, 0)
-        Vertex { pos: [-1,  1, -1], tex_coord: [0, 0] },
         Vertex { pos: [ 1,  1, -1], tex_coord: [1, 0] },
-        Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
+        Vertex { pos: [-1,  1, -1], tex_coord: [0, 0] },
         Vertex { pos: [-1,  1,  1], tex_coord: [0, 1] },
+        Vertex { pos: [ 1,  1,  1], tex_coord: [1, 1] },
         // back (0, -1, 0)
         Vertex { pos: [ 1, -1,  1], tex_coord: [0, 0] },
         Vertex { pos: [-1, -1,  1], tex_coord: [1, 0] },
@@ -179,7 +179,7 @@ fn main() {
          0,  1,  2,  2,  3,  0, // top
          4,  5,  6,  6,  7,  4, // bottom
          8,  9, 10, 10, 11,  8, // right
-        12, 13, 14, 14, 16, 12, // left
+        12, 13, 14, 14, 15, 12, // left
         16, 17, 18, 18, 19, 16, // front
         20, 21, 22, 22, 23, 20, // back
     ];
