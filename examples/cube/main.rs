gfx-rs/gfx

commit 1b36d71172c6a9a16154158734e7b68396c81476

Have Renderer::draw take the batch by-value

src/gfx/lib.rs
@@ -118,7 +118,7 @@ impl<D: device::Device<C>, C: device::draw::CommandBuffer> Graphics<D, C> {
     /// Draw a ref batch.
     pub fn draw<'a, L, T: shade::ShaderParam<L>>(&'a mut self,
         batch: &'a RefBatch<L, T>, data: &'a T, frame: &Frame) {
-        self.renderer.draw(&(batch, data, &self.context), frame)
+        self.renderer.draw((batch, data, &self.context), frame)
     }
 
     /// Submit the internal command buffer and reset for the next frame.

src/render/lib.rs
@@ -180,10 +180,10 @@ impl<C: CommandBuffer> Renderer<C> {
     }
 
     /// Draw a `batch` into the specified `frame`
-    pub fn draw<B: Batch>(&mut self, batch: &B, frame: &target::Frame) {
+    pub fn draw<B: Batch>(&mut self, batch: B, frame: &target::Frame) {
         self.bind_frame(frame);
         let (mesh, link, slice, program, state) = batch.get_data();
-        self.bind_program(batch, program);
+        self.bind_program(&batch, program);
         self.bind_state(state);
         self.bind_mesh(mesh, link, program.get_info());
         self.draw_slice(slice, None);

commit 4919c4b255df7de32d61f3d8b8b7afbee16d1ac1

Have `Render::draw` take batch by reference
`OwnedBatch` broke with
https://github.com/bjz/gfx-rs/commit/1b36d71172c6a9a16154158734e7b68396c
81476

examples/cube/main.rs
@@ -237,7 +237,7 @@ fn main() {
         }
 
         renderer.clear(clear_data, gfx::COLOR | gfx::DEPTH, &frame);
-        renderer.draw((&batch, &data, &context), &frame);
+        renderer.draw(&(&batch, &data, &context), &frame);
         device.submit(renderer.as_buffer());
         renderer.reset();
 

src/gfx/lib.rs
@@ -88,7 +88,7 @@ impl<D: device::Device<C>, C: device::draw::CommandBuffer> Graphics<D, C> {
     /// Draw a ref batch.
     pub fn draw<'a, L, T: shade::ShaderParam<L>>(&'a mut self,
         batch: &'a batch::RefBatch<L, T>, data: &'a T, frame: &Frame) {
-        self.renderer.draw((batch, data, &self.context), frame)
+        self.renderer.draw(&(batch, data, &self.context), frame)
     }
 
     /// Submit the internal command buffer and reset for the next frame.

src/render/lib.rs
@@ -176,10 +176,10 @@ impl<C: CommandBuffer> Renderer<C> {
     }
 
     /// Draw a `batch` into the specified `frame`
-    pub fn draw<B: Batch>(&mut self, batch: B, frame: &target::Frame) {
+    pub fn draw<B: Batch>(&mut self, batch: &B, frame: &target::Frame) {
         self.bind_frame(frame);
         let (mesh, link, slice, program, state) = batch.get_data();
-        self.bind_program(&batch, program);
+        self.bind_program(batch, program);
         self.bind_state(state);
         self.bind_mesh(mesh, link, program.get_info());
         self.draw_slice(slice, None);