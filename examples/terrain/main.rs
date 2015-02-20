e862713f960d1f9186ec15d5495538abe9995ef9
TheBreadCat <TheBreadCat@gmail.com>
2015-02-20 18:26:45 +0100

    Fixed issue #578

--- examples/terrain/main.rs
+++ examples/terrain/main.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(core, plugin)]
+#![feature(core, plugin, custom_attribute)]
 #![plugin(gfx_macros)]
 
 extern crate cgmath;
