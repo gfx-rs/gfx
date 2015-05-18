ba5061dd2526357314e62b403b8ef71620f97e11
Dzmitry Malyshau <kvarkus@gmail.com>
2015-05-17 23:25:45 -0400

    Removed the old init in favor of the stream

--- src/lib.rs
+++ src/lib.rs
@@ -71,13 +71,6 @@ impl<R: gfx::Resources> gfx::Window<R> for Output<R> {
 
 /// Result of successful context initialization.
 pub type Success = (
-    Output<gfx_device_gl::Resources>,
-    gfx_device_gl::Device,
-    gfx_device_gl::Factory,
-);
-
-/// A streamed version of the success struct.
-pub type SuccessStream = (
     gfx::OwnedStream<
         gfx_device_gl::Resources,
         gfx_device_gl::CommandBuffer,
@@ -87,13 +80,13 @@ pub type SuccessStream = (
     gfx_device_gl::Factory,
 );
 
-
 /// Initialize with a window.
 pub fn init(window: glutin::Window) -> Success {
+    use gfx::traits::StreamFactory;
     unsafe { window.make_current() };
     let format = window.get_pixel_format();
     let device = gfx_device_gl::Device::new(|s| window.get_proc_address(s));
-    let factory = device.spawn_factory();
+    let mut factory = device.spawn_factory();
     let out = Output {
         window: window,
         frame: factory.get_main_frame_buffer(),
@@ -103,13 +96,6 @@ pub fn init(window: glutin::Window) -> Success {
         supports_gamma_convertion: format.srgb,
         gamma: gfx::Gamma::Original,
     };
-    (out, device, factory)
-}
-
-/// Initialize with a window, return a `Stream`.
-pub fn init_stream(window: glutin::Window) -> SuccessStream {
-    use gfx::traits::StreamFactory;
-    let (out, device, mut factory) = init(window);
     let stream = factory.create_stream(out);
     (stream, device, factory)
 }
