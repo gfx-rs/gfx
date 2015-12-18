00c5ee8f6b0c1b8f6741773108a96487af6cc46b
sectopod <kvarkus@tut.by>
2015-12-17 23:12:52 -0500

    Format - implemented sub-traits

--- src/core/src/format.rs
+++ src/core/src/format.rs
@@ -17,7 +17,7 @@
 use std::marker::PhantomData;
 
 macro_rules! impl_surface_type {
-    { $($name:ident,)* } => {
+    { $($name:ident $(=$tr:ident)* ,)* } => {
         #[derive(Copy, Clone)]
         #[allow(non_camel_case_types)]
         #[repr(u8)]
@@ -25,24 +25,29 @@ macro_rules! impl_surface_type {
             $( $name, )*
         }
         $(
+            #[allow(non_camel_case_types)]
             pub enum $name {}
             impl SurfaceTyped for $name {
                 fn get_surface_type() -> SurfaceType {
                     SurfaceType::$name
                 }
             }
+            $(
+                impl $tr for $name {}
+            )*
         )*
     }
 }
 
 impl_surface_type! {
-    R8_G8_B8_A8,
-    R16_G16_B16_A16,
-    D24_S8,
+    R8_G8_B8_A8     = BufferSurface = TextureSurface = RenderSurface,
+    R16_G16_B16_A16 = BufferSurface = TextureSurface = RenderSurface,
+    R32_G32_B32_A32 = BufferSurface = TextureSurface = RenderSurface,
+    D24_S8          = TextureSurface = DepthStencilSurface,
 }
 
 macro_rules! impl_channel_type {
-    { $($name:ident,)* } => {
+    { $($name:ident $(=$tr:ident)* ,)* } => {
         #[derive(Copy, Clone)]
         #[repr(u8)]
         pub enum ChannelType {
@@ -55,18 +60,22 @@ macro_rules! impl_channel_type {
                     ChannelType::$name
                 }
             }
+            $(
+                impl $tr for $name {}
+            )*
         )*
     }
 }
 
 impl_channel_type! {
-    Int,
-    Uint,
-    IntScaled,
-    UintScaled,
-    IntNormalized,
-    UintNormalized,
-    Float,
+    Int             = TextureChannel = RenderChannel,
+    Uint            = TextureChannel = RenderChannel,
+    IntScaled       = TextureChannel,
+    UintScaled      = TextureChannel,
+    IntNormalized   = TextureChannel = RenderChannel = BlendChannel,
+    UintNormalized  = TextureChannel = RenderChannel = BlendChannel,
+    Float           = TextureChannel = RenderChannel = BlendChannel,
+    Srgb            = TextureChannel = RenderChannel = BlendChannel,
 }
 
 #[derive(Copy, Clone)]
@@ -94,15 +103,32 @@ pub struct RuntimeFormat(pub SurfaceType, pub ChannelType, pub Swizzle);
 pub trait SurfaceTyped {
     fn get_surface_type() -> SurfaceType;
 }
+pub trait BufferSurface: SurfaceTyped {}
+pub trait TextureSurface: SurfaceTyped {}
+pub trait RenderSurface: SurfaceTyped {}
+pub trait DepthStencilSurface: SurfaceTyped {}
 
 pub trait ChannelTyped {
     fn get_channel_type() -> ChannelType;
 }
+pub trait TextureChannel: ChannelTyped {}
+pub trait RenderChannel: ChannelTyped {}
+pub trait BlendChannel: RenderChannel {}
 
 pub trait Formatted {
     fn get_format() -> RuntimeFormat;
 }
+pub trait BufferFormat: Formatted {}
+pub trait DepthStencilFormat: Formatted {}
+pub trait TextureFormat: Formatted {}
+pub trait RenderFormat: Formatted {}
+pub trait BlendFormat: RenderFormat {}
 
+impl ChannelTyped for () {
+    fn get_channel_type() -> ChannelType {
+        ChannelType::Float
+    }
+}
 
 pub struct CompileFormat<S, C>(PhantomData<(S,C)>);
 
@@ -112,6 +138,13 @@ impl<S: SurfaceTyped, C: ChannelTyped> Formatted for CompileFormat<S, C> {
     }
 }
 
+impl<S: BufferSurface> BufferFormat                         for CompileFormat<S, ()> {}
+impl<S: DepthStencilSurface> DepthStencilFormat             for CompileFormat<S, ()> {}
+impl<S: TextureSurface, C: TextureChannel> TextureFormat    for CompileFormat<S, C> {}
+impl<S: RenderSurface, C: RenderChannel> RenderFormat       for CompileFormat<S, C> {}
+impl<S: RenderSurface, C: BlendChannel> BlendFormat         for CompileFormat<S, C> {}
+
 pub type Rgba8 = CompileFormat<R8_G8_B8_A8, UintNormalized>;
 pub type Rgba16F = CompileFormat<R16_G16_B16_A16, Float>;
+pub type Rgba32F = CompileFormat<R32_G32_B32_A32, Float>;
 pub type DepthStencil = CompileFormat<D24_S8, UintNormalized>;
