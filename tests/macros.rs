1c24e6c230254f601002ef4f8f5667f6196b7942
sectopod <kvarkus@tut.by>
2016-01-02 22:31:18 -0500

    PSO - removed gfx_vertex and gfx_parameters macros

--- tests/macros.rs
+++ tests/macros.rs
@@ -1,19 +1,6 @@
 #[macro_use]
 extern crate gfx;
 
-gfx_vertex!(_Foo {
-    x@ _x: i8,
-    y@ _y: f32,
-    z@ _z: [u32; 4],
-});
-
-gfx_parameters!(_Bar {
-    x@ _x: i32,
-    y@ _y: [f32; 4],
-    b@ _b: gfx::handle::RawBuffer<R>,
-    t@ _t: gfx::shade::TextureParam<R>,
-});
-
 gfx_vertex_struct!(Vertex {
     _x: i8 = "x",
     _y: f32 = "y",
