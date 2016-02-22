b0eaa3abe6a202b98ec8f4bc0b52c63696a3e02c
Kael Choi <kvarkus@tut.by>
2016-02-21 22:50:48 -0500

    App - reworked the launcher mechanics

--- src/lib.rs
+++ src/lib.rs
@@ -30,86 +30,83 @@ pub struct Init<R: gfx::Resources> {
     pub depth: gfx::handle::DepthStencilView<R, DepthFormat>,
 }
 
-pub enum Backend {
-    OpenGL2,
-    Direct3D11,
-}
-
 pub struct Config {
-    backend: Backend,
-    size: (u16, u16),
+    pub size: (u16, u16),
 }
-
-impl Config {
-    pub fn new(back: Backend) -> Config {
-        Config {
-            backend: back,
-            size: (800, 520),
-        }
-    }
-}
-
 
 pub trait Application<R: gfx::Resources> {
     fn new<F: gfx::Factory<R>>(F, Init<R>) -> Self;
     fn render<C: gfx::CommandBuffer<R>>(&mut self, &mut gfx::Encoder<R, C>);
+}
 
-    fn launch(title: &str, config: Config) where
-        Self: Sized + Application<gfx_device_gl::Resources> + Application<gfx_device_dx11::Resources>
-    {
-        use gfx::traits::{Device, Factory, FactoryExt};
+pub trait ApplicationGL2 {
+    fn launch(&str, Config);
+}
+
+pub trait ApplicationD3D11 {
+    fn launch(&str, Config);
+}
+
+impl<A: Application<gfx_device_gl::Resources>> ApplicationGL2 for A {
+    fn launch(title: &str, config: Config) {
+        use gfx::traits::{Device, FactoryExt};
+
         env_logger::init().unwrap();
-        match config.backend {
-            Backend::OpenGL2 => {
-                let builder = glutin::WindowBuilder::new()
-                    .with_title(title.to_string())
-                    .with_dimensions(config.size.0 as u32, config.size.1 as u32)
-                    .with_vsync();
-                let (window, mut device, mut factory, main_color, main_depth) =
-                    gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
-                let mut encoder = factory.create_encoder();
-                let mut app = Self::new(factory, Init {
-                    color: main_color,
-                    depth: main_depth,
-                });
-                'main: loop {
-                    // quit when Esc is pressed.
-                    for event in window.poll_events() {
-                        match event {
-                            glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
-                            glutin::Event::Closed => break 'main,
-                            _ => {},
-                        }
-                    }
-                    // draw a frame
-                    encoder.reset();
-                    app.render(&mut encoder);
-                    device.submit(encoder.as_buffer());
-                    window.swap_buffers().unwrap();
-                    device.cleanup();
+        let builder = glutin::WindowBuilder::new()
+            .with_title(title.to_string())
+            .with_dimensions(config.size.0 as u32, config.size.1 as u32)
+            .with_vsync();
+        let (window, mut device, mut factory, main_color, main_depth) =
+            gfx_window_glutin::init::<ColorFormat, DepthFormat>(builder);
+        let mut encoder = factory.create_encoder();
+        let mut app = Self::new(factory, Init {
+            color: main_color,
+            depth: main_depth,
+        });
+        'main: loop {
+            // quit when Esc is pressed.
+            for event in window.poll_events() {
+                match event {
+                    glutin::Event::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape)) |
+                    glutin::Event::Closed => break 'main,
+                    _ => {},
                 }
-            },
-            Backend::Direct3D11 => {
-                use gfx::tex::Size;
-                let (window, mut device, mut factory, main_color) =
-                    gfx_window_dxgi::init::<ColorFormat>(title, config.size.0, config.size.1)
-                    .unwrap();
-                let mut encoder = factory.create_encoder();
-                let (_, _, main_depth) = factory.create_depth_stencil(
-                    config.size.0 as Size, config.size.1 as Size
-                    ).unwrap();
-                let mut app = Self::new(factory, Init {
-                    color: main_color,
-                    depth: main_depth,
-                });
-                while window.dispatch() {
-                    encoder.reset();
-                    app.render(&mut encoder);
-                    device.submit(encoder.as_buffer());
-                    window.swap_buffers(1);
-                    device.cleanup();
-                }
-            },
+            }
+            // draw a frame
+            encoder.reset();
+            app.render(&mut encoder);
+            device.submit(encoder.as_buffer());
+            window.swap_buffers().unwrap();
+            device.cleanup();
+        }
+    }
+}
+
+impl<A: Application<gfx_device_dx11::Resources>> ApplicationD3D11 for A {
+    fn launch(title: &str, config: Config) {
+        use gfx::traits::{Device, Factory, FactoryExt};
+        use gfx::tex::Size;
+
+        env_logger::init().unwrap();
+        let (window, mut device, mut factory, main_color) =
+            gfx_window_dxgi::init::<ColorFormat>(title, config.size.0, config.size.1)
+            .unwrap();
+        let mut encoder = factory.create_encoder();
+        let (_, _, main_depth) = factory.create_depth_stencil(
+            config.size.0 as Size, config.size.1 as Size
+            ).unwrap();
+
+        let mut app = Self::new(factory, Init {
+            color: main_color,
+            depth: main_depth,
+        });
+
+        while window.dispatch() {
+            encoder.reset();
+            app.render(&mut encoder);
+            device.submit(encoder.as_buffer());
+            window.swap_buffers(1);
+            device.cleanup();
         }
     }
 }
