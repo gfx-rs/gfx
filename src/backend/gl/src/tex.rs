8b6a899836f59829f4ccfb82b6856c85c3f55ce0
Dzmitry Malyshau <kvarkus@gmail.com>
2016-04-11 18:19:32 -0400

    Update tex.rs

--- src/backend/gl/src/tex.rs
+++ src/backend/gl/src/tex.rs
@@ -62,7 +62,7 @@ fn format_to_glpixel(format: NewFormat) -> GLenum {
     match format.0 {
         S::R8 | S::R16 | S::R32=> r,
         S::R4_G4 | S::R8_G8 | S::R16_G16 | S::R32_G32 => rg,
-        S::R16_G16_B16 | S::R32_G32_B32 | S::R5_G6_B5 | S::R11_G11_B10 => rg,
+        S::R16_G16_B16 | S::R32_G32_B32 | S::R5_G6_B5 | S::R11_G11_B10 => rgb,
         S::R8_G8_B8_A8 | S::R16_G16_B16_A16 | S::R32_G32_B32_A32 |
         S::R4_G4_B4_A4 | S::R5_G5_B5_A1 | S::R10_G10_B10_A2 => rgba,
         S::D24_S8 => gl::DEPTH_STENCIL,
