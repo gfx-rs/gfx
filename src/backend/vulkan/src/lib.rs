812fd8722faeba132ea4ebf06b4549a6337096c5
Kael Choi <kvarkus@tut.by>
2016-04-29 22:46:26 -0400

    [vk] basic application info

--- src/backend/vulkan/src/lib.rs
+++ src/backend/vulkan/src/lib.rs
@@ -14,6 +14,9 @@
 
 extern crate gfx_core;
 
+use std::ffi::CString;
+use std::ptr;
+
 mod vk {
     #![allow(dead_code)]
     #![allow(non_upper_case_globals)]
@@ -22,6 +25,68 @@ mod vk {
     include!(concat!(env!("OUT_DIR"), "/vk_bindings.rs"));
 }
 
+
+/// Information that can be given to the Vulkan driver so that it can identify your application.
+pub struct ApplicationInfo<'a> {
+    /// Name of the application.
+    pub application_name: &'a str,
+    /// An opaque number that contains the version number of the application.
+    pub application_version: u32,
+    /// Name of the engine used to power the application.
+    pub engine_name: &'a str,
+    /// An opaque number that contains the version number of the engine.
+    pub engine_version: u32,
+}
+
+
+struct PhysicalDeviceInfo {
+    device: vk::PhysicalDevice,
+    properties: vk::PhysicalDeviceProperties,
+    queue_families: Vec<vk::QueueFamilyProperties>,
+    memory: vk::PhysicalDeviceMemoryProperties,
+    //available_features: Features,
+}
+
 pub struct Backend {
     instance: vk::Instance,
+    pointers: vk::InstancePointers,
+    devices: Vec<PhysicalDeviceInfo>,
+}
+
+pub fn create(app_info: Option<&ApplicationInfo>) -> Backend {
+    let mut c_app_name: CString;
+    let mut c_engine_name: CString;
+    let mut vk_info: vk::ApplicationInfo;
+
+    let info_ptr = if let Some(info) = app_info {
+        c_app_name = CString::new(info.application_name).unwrap();
+        c_engine_name = CString::new(info.engine_name).unwrap();
+        vk_info = vk::ApplicationInfo {
+            sType: vk::STRUCTURE_TYPE_APPLICATION_INFO,
+            pNext: ptr::null(),
+            pApplicationName: c_app_name.as_ptr(),
+            applicationVersion: info.application_version,
+            pEngineName: c_engine_name.as_ptr(),
+            engineVersion: info.engine_version,
+            apiVersion: 0x1000, //TODO
+        };
+        &vk_info as *const _
+    }else {
+        ptr::null()
+    };
+
+    let create_info = vk::InstanceCreateInfo {
+        sType: vk::STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        pNext: ptr::null(),
+        flags: 0,
+        pApplicationInfo: info_ptr,
+        enabledLayerCount: 0, //TODO
+        ppEnabledLayerNames: ptr::null(), //TODO
+        enabledExtensionCount: 0, //TODO
+        ppEnabledExtensionNames: ptr::null(), //TODO
+    };
+
+    Backend {
+
+    }
 }
