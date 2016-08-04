49cf02c22f21274ddda1c830786d5506e0713009
msiglreith <msiglreith@users.noreply.github.com>
2016-08-04 11:11:45 +0200

    Rebuild all example HLSL shaders and equalize the compile scripts for the examples

--- examples/shadow/compile.cmd
+++ examples/shadow/compile.cmd
@@ -1,5 +1,6 @@
-set FXC="%DXSDK_DIR%\Utilities\bin\x64\fxc.exe"
-mkdir data
+@echo off
+set FXC="%DXSDK_DIR%\Utilities\bin\x64\fxc.exe" -nologo
+if not exist data mkdir data
 %FXC% /T vs_4_1 /E Vertex /Fo data/forward_vs.fx shader/forward.hlsl
 %FXC% /T ps_4_1 /E Pixel /Fo data/forward_ps.fx shader/forward.hlsl
 %FXC% /T vs_4_1 /E Vertex /Fo data/shadow_vs.fx shader/shadow.hlsl
