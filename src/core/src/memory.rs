9c1e943357b3006203edc8b050b38a45f3c1b3da
Sven Nilsen <bvssvni@gmail.com>
2017-01-23 16:56:28 +0100

    Implement `Pod` for tuples
    Closes https://github.com/gfx-rs/gfx/issues/1157

--- src/core/src/memory.rs
+++ src/core/src/memory.rs
@@ -100,6 +100,8 @@ impl_pod! { ar =
     0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
 }
 
+unsafe impl<T: Pod, U: Pod> Pod for (T, U) {}
+
 /// Cast a slice from one POD type to another.
 pub fn cast_slice<A: Pod, B: Pod>(slice: &[A]) -> &[B] {
     use std::slice;
