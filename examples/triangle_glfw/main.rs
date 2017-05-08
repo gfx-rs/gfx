ac794d07dfddaf115e0d71fa3fdb985c4079dd65
Mateusz 'Haggus' Mrowiec <matt.mrowiec@gmail.com>
2017-05-08 23:13:40 +0200

    Triangle example using GLFW

---
+++ examples/triangle_glfw/main.rs
@@ -0,0 +1,101 @@
+// Copyright 2015 The Gfx-rs Developers.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#[macro_use]
+extern crate gfx;
+extern crate gfx_window_glfw;
+extern crate glfw;
+
+use gfx::traits::FactoryExt;
+use gfx::Device;
+use glfw::{Action, Context, Key};
+
+pub type ColorFormat = gfx::format::Rgba8;
+pub type DepthFormat = gfx::format::DepthStencil;
+
+gfx_defines!{
+    vertex Vertex {
+        pos: [f32; 2] = "a_Pos",
+        color: [f32; 3] = "a_Color",
+    }
+
+    pipeline pipe {
+        vbuf: gfx::VertexBuffer<Vertex> = (),
+        out: gfx::RenderTarget<ColorFormat> = "Target0",
+    }
+}
+
+const TRIANGLE: [Vertex; 3] = [
+    Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
+    Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
+    Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] }
+];
+
+const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
+
+pub fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)
+        .ok()
+        .expect("Failed to initialize GLFW");
+
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 2));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+
+    let (mut window, events) = glfw.create_window(1024, 768, "Triangle example", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.set_close_polling(true);
+    window.make_current();
+    glfw.set_error_callback(glfw::FAIL_ON_ERRORS);
+    let (mut device, mut factory, main_color, main_depth) =
+        gfx_window_glfw::init(&mut window);
+
+    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+
+    let pso = factory.create_pipeline_simple(
+        include_bytes!("shader/triangle_150.glslv"),
+        include_bytes!("shader/triangle_150.glslf"),
+        pipe::new()
+    ).unwrap();
+
+    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
+
+    let data = pipe::Data {
+        vbuf: vertex_buffer,
+        out: main_color
+    };
+
+    let mut running = true;
+    while running {
+        // fetch events
+        glfw.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => running = false,
+                glfw::WindowEvent::Close => running = false,
+                _ => {},
+            }
+        }
+
+        // draw a frame
+        encoder.clear(&data.out, CLEAR_COLOR);
+        encoder.draw(&slice, &pso, &data);
+        encoder.flush(&mut device);
+        window.swap_buffers();
+        device.cleanup();
+    }
+}
