bdc438e829511971f2070b2e4db19b8746103530
msiglreith <m.siglreith@gmail.com>
2017-06-13 19:46:13 +0200

    [ll] gl: Fix submission, swapchain creation and remove outdated code

--- src/window/glutin/src/lib.rs
+++ src/window/glutin/src/lib.rs
@@ -29,130 +29,11 @@ use glutin::GlContext;
 #[cfg(feature = "headless")]
 mod headless;
 
-/// Initialize with a window builder.
-/// Generically parametrized version over the main framebuffer format.
-///
-/// # Example
-///
-/// ```no_run
-/// extern crate gfx_core;
-/// extern crate gfx_device_gl;
-/// extern crate gfx_window_glutin;
-/// extern crate glutin;
-///
-/// use gfx_core::format::{DepthStencil, Rgba8};
-///
-/// fn main() {
-///     let events_loop = glutin::EventsLoop::new();
-///     let window_builder = glutin::WindowBuilder::new().with_title("Example".to_string());
-///     let context = glutin::ContextBuilder::new();
-///     let (window, device, factory, rtv, stv) =
-///         gfx_window_glutin::init::<Rgba8, DepthStencil>(window_builder, context, &events_loop);
-///
-///     // your code
-/// }
-/// ```
-pub fn init<Cf, Df>(window: glutin::WindowBuilder,
-                    context: glutin::ContextBuilder,
-                    events_loop: &glutin::EventsLoop) ->
-            (glutin::GlWindow, device_gl::Device, device_gl::Factory,
-            handle::RenderTargetView<R, Cf>, handle::DepthStencilView<R, Df>)
-where
-    Cf: format::RenderFormat,
-    Df: format::DepthFormat,
-{
-    let (window, device, factory, color_view, ds_view) = init_raw(
-        window,
-        context,
-        events_loop,
-        Cf::get_format(),
-        Df::get_format());
-    (window, device, factory, Typed::new(color_view), Typed::new(ds_view))
-}
-
-/// Initialize with an existing Glutin window.
-/// Generically parametrized version over the main framebuffer format.
-///
-/// # Example (using Piston to create the window)
-///
-/// ```rust,ignore
-/// extern crate piston;
-/// extern crate glutin_window;
-/// extern crate gfx_window_glutin;
-///
-/// // Create window with Piston
-/// let settings = piston::window::WindowSettings::new("Example", [800, 600]);
-/// let mut glutin_window = glutin_window::GlutinWindow::new(&settings).unwrap();
-///
-/// // Initialise gfx
-/// let (mut device, mut factory, main_color, main_depth) =
-///     gfx_window_glutin::init_existing::<ColorFormat, DepthFormat>(&glutin_window.window);
-///
-/// let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-/// ```
-pub fn init_existing<Cf, Df>(window: &glutin::GlWindow) ->
-            (device_gl::Device, device_gl::Factory,
-            handle::RenderTargetView<R, Cf>, handle::DepthStencilView<R, Df>)
-where
-    Cf: format::RenderFormat,
-    Df: format::DepthFormat,
-{
-    let (device, factory, color_view, ds_view) = init_existing_raw(window, Cf::get_format(), Df::get_format());
-    (device, factory, Typed::new(color_view), Typed::new(ds_view))
-}
-
 fn get_window_dimensions(window: &glutin::GlWindow) -> texture::Dimensions {
     let (width, height) = window.get_inner_size().unwrap();
     let aa = window.get_pixel_format().multisampling
                    .unwrap_or(0) as texture::NumSamples;
     ((width as f32 * window.hidpi_factor()) as texture::Size, (height as f32 * window.hidpi_factor()) as texture::Size, 1, aa.into())
-}
-
-/// Initialize with a window builder. Raw version.
-pub fn init_raw(window: glutin::WindowBuilder,
-                mut context: glutin::ContextBuilder,
-                events_loop: &glutin::EventsLoop,
-                color_format: format::Format,
-                ds_format: format::Format) ->
-                (glutin::GlWindow, device_gl::Device, device_gl::Factory,
-                handle::RawRenderTargetView<R>, handle::RawDepthStencilView<R>)
-{
-    let window = {
-        let color_total_bits = color_format.0.get_total_bits();
-        let alpha_bits = color_format.0.get_alpha_stencil_bits();
-        let depth_total_bits = ds_format.0.get_total_bits();
-        let stencil_bits = ds_format.0.get_alpha_stencil_bits();
-
-        context = context
-            .with_depth_buffer(depth_total_bits - stencil_bits)
-            .with_stencil_buffer(stencil_bits)
-            .with_pixel_format(color_total_bits - alpha_bits, alpha_bits)
-            .with_srgb(color_format.1 == format::ChannelType::Srgb);
-
-        glutin::GlWindow::new(window, context, &events_loop).unwrap()
-    };
-
-    let (device, factory, color_view, ds_view) = init_existing_raw(&window, color_format, ds_format);
-
-    (window, device, factory, color_view, ds_view)
-}
-
-/// Initialize with an existing Glutin window. Raw version.
-pub fn init_existing_raw(window: &glutin::GlWindow,
-                color_format: format::Format, ds_format: format::Format) ->
-                (device_gl::Device, device_gl::Factory,
-                handle::RawRenderTargetView<R>, handle::RawDepthStencilView<R>)
-{
-    unsafe { window.make_current().unwrap() };
-    let (device, factory) = device_gl::create(|s|
-        window.get_proc_address(s) as *const std::os::raw::c_void);
-
-    // create the main color/depth targets
-    let dim = get_window_dimensions(window);
-    let (color_view, ds_view) = device_gl::create_main_targets_raw(dim, color_format.0, ds_format.0);
-
-    // done
-    (device, factory, color_view, ds_view)
 }
 
 /// Update the internal dimensions of the main framebuffer targets. Generic version over the format.
@@ -183,19 +64,6 @@ pub fn update_views_raw(window: &glutin::GlWindow, old_dimensions: texture::Dime
     }
 }
 
-/// Create new main target views based on the current size of the window.
-/// Best called just after a WindowResize event.
-pub fn new_views<Cf, Df>(window: &glutin::GlWindow)
-        -> (handle::RenderTargetView<R, Cf>, handle::DepthStencilView<R, Df>) where
-    Cf: format::RenderFormat,
-    Df: format::DepthFormat,
-{
-    let dim = get_window_dimensions(window);
-    let (color_view_raw, depth_view_raw) =
-        device_gl::create_main_targets_raw(dim, Cf::get_format().0, Df::get_format().0);
-    (Typed::new(color_view_raw), Typed::new(depth_view_raw))
-}
-
 pub struct SwapChain<'a> {
     // Underlying window, required for presentation
     window: &'a glutin::Window,
@@ -222,19 +90,19 @@ impl<'a> core::SwapChain<device_gl::Backend> for SwapChain<'a> {
 
 pub struct Surface<'a> {
     window: &'a glutin::Window,
+    manager: handle::Manager<R>,
 }
 
 impl<'a> core::Surface<device_gl::Backend> for Surface<'a> {
     type SwapChain = SwapChain<'a>;
 
     fn supports_queue(&self, queue_family: &device_gl::QueueFamily) -> bool { true }
-    fn build_swapchain<Q>(&self, config: core::SwapchainConfig, present_queue: &Q) -> SwapChain<'a>
+    fn build_swapchain<Q>(&mut self, config: core::SwapchainConfig, present_queue: &Q) -> SwapChain<'a>
         where Q: AsRef<device_gl::CommandQueue>
     {
         use core::handle::Producer;
         let dim = get_window_dimensions(self.window);
-        let mut temp = handle::Manager::new();
-        let backbuffer = temp.make_texture(
+        let color = self.manager.make_texture(
             device_gl::NewTexture::Surface(0),
             texture::Info {
                 levels: 1,
@@ -245,11 +113,22 @@ impl<'a> core::Surface<device_gl::Backend> for Surface<'a> {
             },
         );
 
-        // TODO: depth stencil
+        let ds = config.depth_stencil_format.map(|ds_format| {
+            self.manager.make_texture(
+                device_gl::NewTexture::Surface(0),
+                texture::Info {
+                    levels: 1,
+                    kind: texture::Kind::D2(dim.0, dim.1, dim.3),
+                    format: ds_format.0,
+                    bind: memory::DEPTH_STENCIL | memory::TRANSFER_SRC,
+                    usage: memory::Usage::Data,
+                },
+            )
+        });
 
         SwapChain {
             window: self.window,
-            backbuffer: [(backbuffer, None); 1],
+            backbuffer: [(color, ds); 1],
         }
     }
 }
@@ -278,7 +157,10 @@ impl<'a> core::WindowExt<device_gl::Backend> for Window<'a> {
     fn get_surface_and_adapters(&mut self) -> (Surface<'a>, Vec<device_gl::Adapter>) {
         unsafe { self.0.make_current().unwrap() };
         let adapter = device_gl::Adapter::new(|s| self.0.get_proc_address(s) as *const std::os::raw::c_void);
-        let surface = Surface { window: self.0 };
+        let surface = Surface {
+            window: self.0,
+            manager: handle::Manager::new(),
+        };
 
         (surface, vec![adapter])
     }
