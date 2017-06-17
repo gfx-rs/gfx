7de6a87c84eb8fcc35ceeb2ff93c45c0ac3a79ee
Matthew McAllister <matthew.mcallister@berkeley.edu>
2017-06-16 19:46:50 -0700

    Fix depth pixel format in GL

--- src/backend/gl/src/tex.rs
+++ src/backend/gl/src/tex.rs
@@ -67,7 +67,7 @@ fn format_to_glpixel(format: NewFormat) -> GLenum {
         S::R8_G8_B8_A8 | S::R16_G16_B16_A16 | S::R32_G32_B32_A32 |
         S::R4_G4_B4_A4 | S::R5_G5_B5_A1 | S::R10_G10_B10_A2 => rgba,
         S::D24_S8 => gl::DEPTH_STENCIL,
-        S::D16 | S::D24 | S::D32 => gl::DEPTH,
+        S::D16 | S::D24 | S::D32 => gl::DEPTH_COMPONENT,
         S::B8_G8_R8_A8 => bgra,
     }
 }
