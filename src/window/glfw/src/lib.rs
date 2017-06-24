b94d71bbd82a31f90b046963dbff6b758e984e4d
msiglreith <m.siglreith@gmail.com>
2017-06-24 18:46:11 +0200

    [ll] Update glfw and sdl window, fix vk linux build
    Adapt glfw and sdl window creation to the new model (Window-> Surface -> SwapChain).
    The swapchains of these backends are restricted as mentioned in #1304
    
    * Update glfw window and example
    * Update SDL2 window and example
    * Improve sdl window documentation, fix glfw doc example
    * linux: fix vulkan surface creation

--- src/window/glfw/src/lib.rs
+++ src/window/glfw/src/lib.rs
@@ -12,62 +12,138 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[deny(missing_docs)]
+//! Initialize with a window.
+//!
+//! # Example
+//!
+//! ```no_run
+//! extern crate gfx_window_glfw;
+//! extern crate glfw;
+//! extern crate gfx_core;
+//!
+//! use gfx_core::WindowExt;
+//!
+//! fn main() {
+//!     use glfw::Context;
+//!
+//!     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)
+//!         .ok().expect("Failed to initialize GLFW");
+//!
+//!     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 2));
+//!     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+//!     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+//!
+//!     let (window, events) = glfw
+//!         .create_window(800, 600, "Example", glfw::WindowMode::Windowed)
+//!         .expect("Failed to create GLFW window.");
+//!
+//!     let mut window = gfx_window_glfw::Window::new(window);
+//!     glfw.set_error_callback(glfw::FAIL_ON_ERRORS);
+//!     let (surface, adapters) = window.get_surface_and_adapters();
+//!
+//!     // some code...
+//! }
+//! ```
 
 extern crate gfx_core as core;
 extern crate gfx_device_gl as device_gl;
 extern crate glfw;
 
+use std::rc::Rc;
+use std::cell::RefCell;
 use core::format::{Rgba8, DepthStencil, SurfaceType};
-use core::handle;
-use core::memory::Typed;
-use core::texture::{AaMode, Size};
+use core::{handle, memory};
+use core::texture::{self, AaMode, Size};
 use glfw::Context;
 
-/// Initialize with a window.
-///
-/// # Example
-///
-/// ```no_run
-/// extern crate gfx_window_glfw;
-/// extern crate glfw;
-/// 
-/// fn main() {
-///     use glfw::Context;
-///
-///     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS)
-///         .ok().expect("Failed to initialize GLFW");
-/// 
-///     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 2));
-///     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
-///     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-/// 
-///     let (mut window, events) = glfw
-///         .create_window(800, 600, "Example", glfw::WindowMode::Windowed)
-///         .expect("Failed to create GLFW window.");
-/// 
-///     window.make_current();
-///     glfw.set_error_callback(glfw::FAIL_ON_ERRORS);
-///     let (device, mut factory, color_view, depth_view) =
-///         gfx_window_glfw::init(&mut window);
-///
-///     // some code...
-/// }
-/// ```
-pub fn init(window: &mut glfw::Window) ->
-    (device_gl::Device,
-     device_gl::Factory,
-     handle::RenderTargetView<device_gl::Resources, Rgba8>,
-     handle::DepthStencilView<device_gl::Resources, DepthStencil>)
-{
-    window.make_current();
-    let (device, factory) = device_gl::create(|s|
-        window.get_proc_address(s) as *const std::os::raw::c_void);
-    // create the main color/depth targets
-    let (width, height) = window.get_framebuffer_size();
-    let dim = (width as Size, height as Size, 1, AaMode::Single);
-    let (color_view, ds_view) = device_gl::create_main_targets_raw(
-        dim, SurfaceType::R8_G8_B8_A8, SurfaceType::D24);
-    // done
-    (device, factory, Typed::new(color_view), Typed::new(ds_view))
+pub struct SwapChain {
+    // Underlying window, required for presentation
+    window: Rc<RefCell<glfw::Window>>,
+    // Single element backbuffer
+    backbuffer: [core::Backbuffer<device_gl::Backend>; 1],
+}
+
+impl<'a> core::SwapChain<device_gl::Backend> for SwapChain {
+    fn get_backbuffers(&mut self) -> &[core::Backbuffer<device_gl::Backend>] {
+        &self.backbuffer
+    }
+
+    fn acquire_frame(&mut self, sync: core::FrameSync<device_gl::Resources>) -> core::Frame {
+        // TODO: fence sync
+        core::Frame::new(0)
+    }
+
+    fn present<Q>(&mut self, _: &mut Q)
+        where Q: AsMut<device_gl::CommandQueue> {
+        self.window.borrow_mut().swap_buffers();
+    }
+}
+
+pub struct Surface {
+    window: Rc<RefCell<glfw::Window>>,
+    manager: handle::Manager<device_gl::Resources>,
+}
+
+impl<'a> core::Surface<device_gl::Backend> for Surface {
+    type SwapChain = SwapChain;
+
+    fn supports_queue(&self, _: &device_gl::QueueFamily) -> bool { true }
+    fn build_swapchain<Q>(&mut self, config: core::SwapchainConfig, _: &Q) -> SwapChain
+        where Q: AsRef<device_gl::CommandQueue>
+    {
+        use core::handle::Producer;
+        let (width, height) = self.window.borrow_mut().get_framebuffer_size();
+        let dim = (width as Size, height as Size, 1, AaMode::Single);
+        let color = self.manager.make_texture(
+            device_gl::NewTexture::Surface(0),
+            texture::Info {
+                levels: 1,
+                kind: texture::Kind::D2(dim.0, dim.1, dim.3),
+                format: config.color_format.0,
+                bind: memory::RENDER_TARGET | memory::TRANSFER_SRC,
+                usage: memory::Usage::Data,
+            },
+        );
+
+        let ds = config.depth_stencil_format.map(|ds_format| {
+            self.manager.make_texture(
+                device_gl::NewTexture::Surface(0),
+                texture::Info {
+                    levels: 1,
+                    kind: texture::Kind::D2(dim.0, dim.1, dim.3),
+                    format: ds_format.0,
+                    bind: memory::DEPTH_STENCIL | memory::TRANSFER_SRC,
+                    usage: memory::Usage::Data,
+                },
+            )
+        });
+
+        SwapChain {
+            window: self.window.clone(),
+            backbuffer: [(color, ds); 1],
+        }
+    }
+}
+
+pub struct Window(pub Rc<RefCell<glfw::Window>>);
+impl Window {
+    pub fn new(window: glfw::Window) -> Self {
+        Window(Rc::new(RefCell::new(window)))
+    }
+}
+
+impl<'a> core::WindowExt<device_gl::Backend> for Window {
+    type Surface = Surface;
+    type Adapter = device_gl::Adapter;
+
+    fn get_surface_and_adapters(&mut self) -> (Surface, Vec<device_gl::Adapter>) {
+        self.0.borrow_mut().make_current();
+        let adapter = device_gl::Adapter::new(|s| self.0.borrow_mut().get_proc_address(s) as *const std::os::raw::c_void);
+        let surface = Surface {
+            window: self.0.clone(),
+            manager: handle::Manager::new(),
+        };
+
+        (surface, vec![adapter])
+    }
 }
