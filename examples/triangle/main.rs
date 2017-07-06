f2cae1f163e418f15eccd06a09ddef5723109da2
msiglreith <m.siglreith@gmail.com>
2017-07-06 18:23:52 +0200

    [ll] Update triangle example

--- examples/triangle/main.rs
+++ examples/triangle/main.rs
@@ -14,9 +14,15 @@
 
 #[macro_use]
 extern crate gfx;
+extern crate gfx_device_gl as device_gl;
 extern crate gfx_window_glutin;
 extern crate glutin;
 
+use gfx::{Adapter, Factory, FrameSync, GraphicsCommandPool, GraphicsPoolExt,
+          Surface, SwapChain, WindowExt};
+use gfx::format::Formatted;
+use gfx::texture;
+use gfx::memory::Typed;
 use gfx::traits::FactoryExt;
 use glutin::GlContext;
 
@@ -44,53 +50,93 @@ const TRIANGLE: [Vertex; 3] = [
 const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
 
 pub fn main() {
-    let mut events_loop = glutin::EventsLoop::new();
-    let window_config = glutin::WindowBuilder::new()
+    // Create window
+    let events_loop = glutin::EventsLoop::new();
+    let builder = glutin::WindowBuilder::new()
         .with_title("Triangle example".to_string())
-        .with_dimensions(1024, 768);
-    let context = glutin::ContextBuilder::new()
-        .with_vsync(true);
-    let (window, mut device, mut factory, main_color, mut main_depth) =
-        gfx_window_glutin::init::<ColorFormat, DepthFormat>(window_config, context, &events_loop);
-    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
-    let pso = factory.create_pipeline_simple(
+        .with_dimensions(1024, 768)
+        .with_vsync();
+    let window = gfx_window_glutin::build(builder, &events_loop, ColorFormat::get_format(), DepthFormat::get_format());
+
+    // Acquire surface and adapters
+    let (mut surface, adapters) = gfx_window_glutin::Window(&window).get_surface_and_adapters();
+    let queue_descs = adapters[0].get_queue_families().iter()
+                                 .filter(|family| surface.supports_queue(&family) )
+                                 .map(|family| { (family, 1) })
+                                 .collect::<Vec<_>>();
+
+    // Open device (factory and queues)
+    let gfx::Device { mut factory, mut general_queues, mut graphics_queues, .. } = adapters[0].open(&queue_descs);
+    let mut graphics_queue = if let Some(queue) = general_queues.first_mut() {
+        queue.as_mut().into()
+    } else if let Some(queue) = graphics_queues.first_mut() {
+        queue.as_mut()
+    } else {
+        panic!("Unable to find a matching general or graphics queue.");
+    };
+
+    // Create swapchain
+    let config = gfx::SwapchainConfig::new()
+                    .with_color::<ColorFormat>();
+    let mut swap_chain = surface.build_swapchain(config, &graphics_queue);
+
+    let views: Vec<gfx::handle::RenderTargetView<device_gl::Resources, ColorFormat>> =
+        swap_chain
+            .get_backbuffers()
+            .iter()
+            .map(|&(ref color, ref ds)| {
+                let color_desc = texture::RenderDesc {
+                    channel: ColorFormat::get_format().1,
+                    level: 0,
+                    layer: None,
+                };
+                let rtv = factory.view_texture_as_render_target_raw(color, color_desc)
+                                 .unwrap();
+                Typed::new(rtv)
+            })
+            .collect();
+
+   let pso = factory.create_pipeline_simple(
         include_bytes!("shader/triangle_150.glslv"),
         include_bytes!("shader/triangle_150.glslf"),
         pipe::new()
     ).unwrap();
     let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
+    let mut graphics_pool = <device_gl::Backend as gfx::Backend>::GraphicsCommandPool::from_queue(graphics_queue.as_ref(), 1);
+    let semaphore = factory.create_semaphore();
+
     let mut data = pipe::Data {
         vbuf: vertex_buffer,
-        out: main_color
+        out: views[0].clone(),
     };
 
-
+    // main loop
     let mut running = true;
     while running {
         // fetch events
-        events_loop.poll_events(|event| {
-            if let glutin::Event::WindowEvent { event, .. } = event {
-                match event {
-                    glutin::WindowEvent::KeyboardInput {
-                        input: glutin::KeyboardInput {
-                            virtual_keycode: Some(glutin::VirtualKeyCode::Escape),
-                            .. },
-                        ..
-                    } | glutin::WindowEvent::Closed => running = false,
-                    glutin::WindowEvent::Resized(width, height) => {
-                        window.resize(width, height);
-                        gfx_window_glutin::update_views(&window, &mut data.out, &mut main_depth);
-                    },
-                    _ => (),
-                }
+        events_loop.poll_events(|glutin::Event::WindowEvent{window_id: _, event}| {
+            match event {
+                glutin::WindowEvent::KeyboardInput(_, _, Some(glutin::VirtualKeyCode::Escape), _) |
+                glutin::WindowEvent::Closed => running = false,
+                glutin::WindowEvent::Resized(_width, _height) => {
+                    // TODO
+                },
+                _ => {},
             }
         });
 
+        // Get next frame
+        let frame = swap_chain.acquire_frame(FrameSync::Semaphore(&semaphore));
+        data.out = views[frame.id()].clone();        
+
         // draw a frame
+        let mut encoder = graphics_pool.acquire_graphics_encoder();
         encoder.clear(&data.out, CLEAR_COLOR);
         encoder.draw(&slice, &pso, &data);
-        encoder.flush(&mut device);
-        window.swap_buffers().unwrap();
-        device.cleanup();
+        encoder.synced_flush(&mut graphics_queue, &[&semaphore], &[], None);
+
+        // present
+        swap_chain.present(&mut graphics_queue);
+        // factory.cleanup();
     }
 }
