0ca1fd1370e0aa78ebd30e87c0eb010dd6320f66
msiglreith <m.siglreith@gmail.com>
2017-07-06 22:54:06 +0200

    [ll] render: Add SwapChain extension trait for easier rtv creation

--- examples/triangle/main.rs
+++ examples/triangle/main.rs
@@ -19,7 +19,7 @@ extern crate gfx_window_glutin;
 extern crate glutin;
 
 use gfx::{Adapter, Factory, FrameSync, GraphicsCommandPool, GraphicsPoolExt,
-          Surface, SwapChain, WindowExt};
+          Surface, SwapChain, SwapChainExt, WindowExt};
 use gfx::format::Formatted;
 use gfx::texture;
 use gfx::memory::Typed;
@@ -79,24 +79,9 @@ pub fn main() {
     let config = gfx::SwapchainConfig::new()
                     .with_color::<ColorFormat>();
     let mut swap_chain = surface.build_swapchain(config, &graphics_queue);
+    let views = swap_chain.create_color_views(&mut factory);
 
-    let views: Vec<gfx::handle::RenderTargetView<device_gl::Resources, ColorFormat>> =
-        swap_chain
-            .get_backbuffers()
-            .iter()
-            .map(|&(ref color, ref ds)| {
-                let color_desc = texture::RenderDesc {
-                    channel: ColorFormat::get_format().1,
-                    level: 0,
-                    layer: None,
-                };
-                let rtv = factory.view_texture_as_render_target_raw(color, color_desc)
-                                 .unwrap();
-                Typed::new(rtv)
-            })
-            .collect();
-
-   let pso = factory.create_pipeline_simple(
+    let pso = factory.create_pipeline_simple(
         include_bytes!("shader/triangle_150.glslv"),
         include_bytes!("shader/triangle_150.glslf"),
         pipe::new()
