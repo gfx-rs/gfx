8d3ef8a35289df1872b9ba5e4d909c4fd65a3565
msiglreith <m.siglreith@gmail.com>
2017-07-12 15:15:11 +0200

    [ll] First update for performance example and add missing command pool functions

--- examples/gamma/main.rs
+++ examples/gamma/main.rs
@@ -54,9 +54,10 @@ pub fn main() {
         .with_dimensions(1024, 768);
     let context = glutin::ContextBuilder::new()
         .with_vsync(true);
-    let window = gfx_window_glutin::build(builder, &events_loop, ColorFormat::get_format(), DepthFormat::get_format());
+    let win = gfx_window_glutin::build(builder, &events_loop, ColorFormat::get_format(), DepthFormat::get_format());
+    let mut window = gfx_window_glutin::Window::new(window);
 
-    let (mut surface, adapters) = gfx_window_glutin::Window(&window).get_surface_and_adapters();
+    let (mut surface, adapters) = window.get_surface_and_adapters();
     let queue_descs = adapters[0].get_queue_families().iter()
                                  .filter(|family| surface.supports_queue(&family) )
                                  .map(|family| { (family, 1) })
@@ -105,6 +106,9 @@ pub fn main() {
                 _ => {},
             }
         });
+
+        // TODO: Need to ensure that it's not in-use (but that's not an issue on GL) 
+        graphics_pool.reset();
 
         // Get next frame
         let frame = swap_chain.acquire_frame(FrameSync::Semaphore(&frame_semaphore));
