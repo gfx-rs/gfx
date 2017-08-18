1f24022bd42346fdb3995909d81f52d09d391dbd
msiglreith <m.siglreith@gmail.com>
2017-08-18 16:23:23 +0200

    [ll] gl: Reset indirect draw buffer

--- src/backend/gl/src/lib.rs
+++ src/backend/gl/src/lib.rs
@@ -475,6 +475,9 @@ impl CommandQueue {
             self.state.vao = true
         }
 
+        // Reset indirect draw buffer
+        unsafe { gl.BindBuffer(gl::DRAW_INDIRECT_BUFFER, 0) };
+
         // Unbind index buffers
         match self.state.index_buffer {
             Some(0) => (), // Nothing to do
@@ -838,9 +841,6 @@ impl CommandQueue {
                 }else if false {
                     error!("Separate blending slots are not supported");
                 }
-            },
-            Command::SetBlendColor(color) => {
-                state::set_blend_color(&self.share.context, color);
             },
             Command::SetPatches(num) => {
                 let gl = &self.share.context;
