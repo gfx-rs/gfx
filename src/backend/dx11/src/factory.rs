4ac356df7305d1a96eb8b059ff0ea09d0712be8d
snuk182 <snuk188@gmail.com>
2017-08-30 22:17:30 +0200

    Mipmap generation for GL

--- src/backend/dx11/src/factory.rs
+++ src/backend/dx11/src/factory.rs
@@ -599,7 +599,7 @@ impl core::Factory<R> for Factory {
     }
 
     fn create_texture_raw(&mut self, desc: texture::Info, hint: Option<core::format::ChannelType>,
-                          data_opt: Option<&[&[u8]]>) -> Result<h::RawTexture<R>, texture::CreationError> {
+                          data_opt: Option<(&[&[u8]], texture::Mipmap)>) -> Result<h::RawTexture<R>, texture::CreationError> {
         use core::texture::{AaMode, CreationError, Kind};
         use data::{map_bind, map_usage, map_surface, map_format};
 
@@ -626,7 +626,7 @@ impl core::Factory<R> for Factory {
 
         self.sub_data_array.clear();
         if let Some(data) = data_opt {
-            for sub in data.iter() {
+            for sub in data.0.iter() {
                 self.sub_data_array.push(winapi::D3D11_SUBRESOURCE_DATA {
                     pSysMem: sub.as_ptr() as *const c_void,
                     SysMemPitch: 0,
