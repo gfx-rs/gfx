35c266e1ca73969bd70cf1dbdcd3a9293e2c249f
msiglreith <m.siglreith@gmail.com>
2017-08-30 18:12:13 +0200

    [ll] gl: Implement fence and sampler handling

--- src/backend/gl/src/device.rs
+++ src/backend/gl/src/device.rs
@@ -1,14 +1,15 @@
 use std::rc::Rc;
 use std::{slice, ptr};
 
-use {gl};
+use gl;
+use gl::types::{GLenum, GLuint, GLint, GLfloat, GLsizei, GLvoid};
 use core::{self as c, device as d, image as i, pass, pso, buffer, mapping};
 use core::memory::{self, Bind, SHADER_RESOURCE, UNORDERED_ACCESS, Typed};
 use core::format::{ChannelType, Format};
 use core::target::{Layer, Level};
 
 use {Info, Backend as B, Share};
-use {conv, device, native as n, pool};
+use {conv, device, native as n, pool, state};
 
 fn access_to_map_bits(access: memory::Access) -> gl::types::GLenum {
     let mut r = 0;
@@ -76,8 +77,8 @@ impl d::Device<B> for Device {
         &self.share.limits
     }
 
-    fn create_heap(&mut self, _: &c::HeapType, _: d::ResourceHeapType, _: u64) -> Result<n::Heap, d::ResourceHeapError> {
-        unimplemented!()
+    fn create_heap(&mut self, heap_type: &c::HeapType, resource_type: d::ResourceHeapType, size: u64) -> Result<n::Heap, d::ResourceHeapError> {
+        Ok(n::Heap)
     }
 
     fn create_renderpass(&mut self, _: &[pass::Attachment], _: &[pass::SubpassDesc], _: &[pass::SubpassDependency]) -> n::RenderPass {
@@ -88,10 +89,12 @@ impl d::Device<B> for Device {
         unimplemented!()
     }
 
-    fn create_graphics_pipelines<'a>(&mut self, _: &[(&n::ShaderLib, &n::PipelineLayout, pass::SubPass<'a, B>, &pso::GraphicsPipelineDesc)])
-            -> Vec<Result<n::GraphicsPipeline, pso::CreationError>> {
-                unimplemented!()
-            }
+    fn create_graphics_pipelines<'a>(
+        &mut self,
+        descs: &[(&n::ShaderLib, &n::PipelineLayout, pass::SubPass<'a, B>, &pso::GraphicsPipelineDesc)],
+    ) -> Vec<Result<n::GraphicsPipeline, pso::CreationError>> {
+        unimplemented!()
+    }
 
     fn create_compute_pipelines(&mut self, _: &[(&n::ShaderLib, pso::EntryPoint, &n::PipelineLayout)]) -> Vec<Result<n::ComputePipeline, pso::CreationError>> {
         unimplemented!()
@@ -107,9 +110,69 @@ impl d::Device<B> for Device {
         unimplemented!()
     }
 
-    fn create_sampler(&mut self, info: i::SamplerInfo) -> n::Sampler {
-        unimplemented!()
+    fn create_sampler(&mut self, info: i::SamplerInfo) -> n::FatSampler {
+        let object = if self.share.features.sampler_objects {
+            let gl = &self.share.context;
+            let mut name = 0 as n::Sampler;
+
+            let (min, mag) = conv::filter_to_gl(info.filter);
+
+            unsafe {
+                gl.GenSamplers(1, &mut name);
+
+                match info.filter{
+                    i::FilterMethod::Anisotropic(fac) if fac > 1 => {
+                        if self.share.private_caps.sampler_anisotropy_ext {
+                            gl.SamplerParameterf(name, gl::TEXTURE_MAX_ANISOTROPY_EXT, fac as GLfloat);
+                        } else if self.share.features.sampler_anisotropy {
+                            // TODO: Uncomment once `gfx_gl` supports GL 4.6
+                            // gl.SamplerParameterf(name, gl::TEXTURE_MAX_ANISOTROPY, fac as GLfloat);
+                        }
+                    }
+                    _ => ()
+                }
+
+                gl.SamplerParameteri(name, gl::TEXTURE_MIN_FILTER, min as GLint);
+                gl.SamplerParameteri(name, gl::TEXTURE_MAG_FILTER, mag as GLint);
+
+                let (s, t, r) = info.wrap_mode;
+                gl.SamplerParameteri(name, gl::TEXTURE_WRAP_S, conv::wrap_to_gl(s) as GLint);
+                gl.SamplerParameteri(name, gl::TEXTURE_WRAP_T, conv::wrap_to_gl(t) as GLint);
+                gl.SamplerParameteri(name, gl::TEXTURE_WRAP_R, conv::wrap_to_gl(r) as GLint);
+
+                if self.share.features.sampler_lod_bias {
+                    gl.SamplerParameterf(name, gl::TEXTURE_LOD_BIAS, info.lod_bias.into());
+                }
+                if self.share.features.sampler_border_color {
+                    let border: [f32; 4] = info.border.into();
+                    gl.SamplerParameterfv(name, gl::TEXTURE_BORDER_COLOR, &border[0]);
+                }
+
+                let (min, max) = info.lod_range;
+                gl.SamplerParameterf(name, gl::TEXTURE_MIN_LOD, min.into());
+                gl.SamplerParameterf(name, gl::TEXTURE_MAX_LOD, max.into());
+
+                match info.comparison {
+                    None => gl.SamplerParameteri(name, gl::TEXTURE_COMPARE_MODE, gl::NONE as GLint),
+                    Some(cmp) => {
+                        gl.SamplerParameteri(name, gl::TEXTURE_COMPARE_MODE, gl::COMPARE_REF_TO_TEXTURE as GLint);
+                        gl.SamplerParameteri(name, gl::TEXTURE_COMPARE_FUNC, state::map_comparison(cmp) as GLint);
+                    }
+                }
+            }
+
+            name
+        } else {
+            0
+        };
+
+        if let Err(err) = self.share.check() {
+            panic!("Error {:?} creating sampler: {:?}", err, info)
+        }
+
+        n::FatSampler { object, info }
     }
+
     fn create_buffer(&mut self, _: u64, _: u64, _: buffer::Usage) -> Result<device::UnboundBuffer, buffer::CreationError> {
         unimplemented!()
     }
@@ -150,13 +213,14 @@ impl d::Device<B> for Device {
     fn view_image_as_unordered_access(&mut self, _: &n::Image, _: Format) -> Result<n::UnorderedAccessView, d::TargetViewError> {
         unimplemented!()
     }
+
     fn create_descriptor_pool(&mut self, _: usize, _: &[pso::DescriptorRangeDesc]) -> n::DescriptorPool {
         unimplemented!()
     }
+
     fn create_descriptor_set_layout(&mut self, _: &[pso::DescriptorSetLayoutBinding]) -> n::DescriptorSetLayout {
         unimplemented!()
     }
-
 
     fn update_descriptor_sets(&mut self, _: &[pso::DescriptorSetWrite<B>]) {
         unimplemented!()
@@ -175,18 +239,82 @@ impl d::Device<B> for Device {
         }
 
     fn create_semaphore(&mut self) -> n::Semaphore {
-        unimplemented!()
+        n::Semaphore
     }
 
-    fn create_fence(&mut self, _: bool) -> n::Fence {
-        unimplemented!()
+    fn create_fence(&mut self, signalled: bool) -> n::Fence {
+        let sync = if signalled && self.share.private_caps.sync_supported {
+            let gl = &self.share.context;
+            unsafe { gl.FenceSync(gl::SYNC_GPU_COMMANDS_COMPLETE, 0) }
+        } else {
+            ptr::null()
+        };
+        n::Fence::new(sync)
     }
 
-    fn reset_fences(&mut self, _: &[&n::Fence]) {
-        unimplemented!()
+    fn reset_fences(&mut self, fences: &[&n::Fence]) {
+        if !self.share.private_caps.sync_supported {
+            return
+        }
+
+        let gl = &self.share.context;
+        for fence in fences {
+            let sync = fence.0.get();
+            unsafe {
+                if gl.IsSync(sync) == gl::TRUE {
+                    gl.DeleteSync(sync);
+                }
+            }
+            fence.0.set(ptr::null())
+        }
     }
-    fn wait_for_fences(&mut self, _: &[&n::Fence], _: d::WaitFor, _: u32) -> bool {
-        unimplemented!()
+
+    fn wait_for_fences(&mut self, fences: &[&n::Fence], wait: d::WaitFor, timeout_ms: u32) -> bool {
+        if !self.share.private_caps.sync_supported {
+            return true;
+        }
+
+        match wait {
+            d::WaitFor::All => {
+                for fence in fences {
+                    match wait_fence(fence, &self.share.context, timeout_ms) {
+                        gl::TIMEOUT_EXPIRED => return false,
+                        gl::WAIT_FAILED => {
+                            if let Err(err) = self.share.check() {
+                                error!("Error when waiting on fence: {:?}", err);
+                            }
+                            return false
+                        }
+                        _ => (),
+                    }
+                }
+                // All fences have indicated a positive result
+                true
+            },
+            d::WaitFor::Any => {
+                let mut waiting = |timeout_ms: u32| {
+                    for fence in fences {
+                        match wait_fence(fence, &self.share.context, 0) {
+                            gl::ALREADY_SIGNALED | gl::CONDITION_SATISFIED => return true,
+                            gl::WAIT_FAILED => {
+                                if let Err(err) = self.share.check() {
+                                    error!("Error when waiting on fence: {:?}", err);
+                                }
+                                return false
+                            }
+                            _ => (),
+                        }
+                    }
+                    // No fence has indicated a postive result
+                    false
+                };
+
+                // Short-circuit:
+                //   Check current state of all fences first,
+                //   else go trough each fence and wait til at least one has finished
+                waiting(0) || waiting(timeout_ms)
+            },
+        }
     }
 
     fn destroy_heap(&mut self, _: n::Heap) {
@@ -240,7 +368,7 @@ impl d::Device<B> for Device {
         unimplemented!()
     }
 
-    fn destroy_sampler(&mut self, _: n::Sampler) {
+    fn destroy_sampler(&mut self, _: n::FatSampler) {
         unimplemented!()
     }
 
