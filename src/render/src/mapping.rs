86b5706a7d798b5fd3de49f325de363f94610386
Thomas Koehler <basta.t.k+git@gmail.com>
2017-09-15 22:31:09 +0200

    [ll] Mapping sugar

--- src/render/src/mapping.rs
+++ src/render/src/mapping.rs
@@ -2,7 +2,7 @@ use std::{fmt, ops};
 use std::error::Error as StdError;
 
 use {core, memory, buffer};
-use Backend;
+use {Backend, Device};
 
 /// Error accessing a mapping.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
@@ -45,28 +45,65 @@ impl StdError for Error {
     }
 }
 
-pub struct Reader<'a, B: Backend, T: 'a + Copy> {
+pub struct Reader<'a, B: Backend, T: 'a> {
     pub(crate) inner: core::mapping::Reader<'a, B, T>,
     pub(crate) info: &'a buffer::Info,
 }
 
-impl<'a, B: Backend, T: 'a + Copy> ops::Deref for Reader<'a, B, T> {
+impl<'a, B: Backend, T: 'a> ops::Deref for Reader<'a, B, T> {
     type Target = [T];
 
     fn deref(&self) -> &[T] { &self.inner }
 }
 
-pub struct Writer<'a, B: Backend, T: 'a + Copy> {
+pub struct Writer<'a, B: Backend, T: 'a> {
     pub(crate) inner: core::mapping::Writer<'a, B, T>,
     pub(crate) info: &'a buffer::Info,
 }
 
-impl<'a, B: Backend, T: 'a + Copy> ops::Deref for Writer<'a, B, T> {
+impl<'a, B: Backend, T: 'a> ops::Deref for Writer<'a, B, T> {
     type Target = [T];
 
     fn deref(&self) -> &[T] { &self.inner }
 }
 
-impl<'a, B: Backend, T: 'a + Copy> ops::DerefMut for Writer<'a, B, T> {
+impl<'a, B: Backend, T: 'a> ops::DerefMut for Writer<'a, B, T> {
     fn deref_mut(&mut self) -> &mut [T] { &mut self.inner }
+}
+
+
+pub struct ReadScope<'a, B: Backend, T: 'a> {
+    pub(crate) reader: Option<Reader<'a, B, T>>,
+    pub(crate) device: &'a mut Device<B>,
+}
+
+impl<'a, B: Backend, T: 'a> ops::Deref for ReadScope<'a, B, T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] { self.reader.as_ref().unwrap() }
+}
+
+impl<'a, B: Backend, T: 'a> Drop for ReadScope<'a, B, T> {
+    fn drop(&mut self) {
+        self.device.release_mapping_reader(self.reader.take().unwrap());
+    }
+}
+
+pub struct WriteScope<'a, B: Backend, T: 'a> {
+    pub(crate) writer: Option<Writer<'a, B, T>>,
+    pub(crate) device: &'a mut Device<B>,
+}
+
+impl<'a, B: Backend, T: 'a> ops::Deref for WriteScope<'a, B, T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] { self.writer.as_ref().unwrap() }
+}
+
+impl<'a, B: Backend, T: 'a> ops::DerefMut for WriteScope<'a, B, T> {
+    fn deref_mut(&mut self) -> &mut [T] { self.writer.as_mut().unwrap() }
+}
+
+impl<'a, B: Backend, T: 'a> Drop for WriteScope<'a, B, T> {
+    fn drop(&mut self) {
+        self.device.release_mapping_writer(self.writer.take().unwrap());
+    }
 }
