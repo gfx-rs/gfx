aeb20ded8dfa91ddc870d776b5ded45ac51845fc
msiglreith <m.siglreith@gmail.com>
2018-01-22 14:33:14 +0100

    dx12: Fix initial fence value

--- src/backend/dx12/src/device.rs
+++ src/backend/dx12/src/device.rs
@@ -1750,9 +1750,7 @@ impl d::Device<B> for Device {
                                 BufferLocation: unsafe { (*buffer.resource).GetGPUVirtualAddress() },
                                 SizeInBytes: size as _,
                             };
-                            unsafe {
-                                raw.CreateConstantBufferView(&desc, handle);
-                            }
+                            unsafe { raw.CreateConstantBufferView(&desc, handle); }
                             handle
                         }));
                     }
@@ -1918,11 +1916,11 @@ impl d::Device<B> for Device {
         }
     }
 
-    fn create_fence(&self, _signaled: bool) -> n::Fence {
+    fn create_fence(&self, signalled: bool) -> n::Fence {
         let mut handle = ptr::null_mut();
         assert_eq!(winerror::S_OK, unsafe {
             self.raw.clone().CreateFence(
-                0,
+                if signalled { 1 } else { 0 },
                 d3d12::D3D12_FENCE_FLAG_NONE,
                 &d3d12::IID_ID3D12Fence,
                 &mut handle,
