7209eef3ef04b5a90d24aea393cfa14a41f26827
msiglreith <m.siglreith@gmail.com>
2018-05-01 15:58:31 +0200

    dx12: Adjust framebuffer sample count

--- src/backend/dx12/src/lib.rs
+++ src/backend/dx12/src/lib.rs
@@ -919,9 +919,9 @@ impl hal::Instance for Instance {
                     min_texel_buffer_offset_alignment: 1, // TODO
                     min_uniform_buffer_offset_alignment: 256, // Required alignment for CBVs
                     min_storage_buffer_offset_alignment: 1, // TODO
-                    framebuffer_color_samples_count: 0b111,
-                    framebuffer_depth_samples_count: 0b111,
-                    framebuffer_stencil_samples_count: 0b111,
+                    framebuffer_color_samples_count: 0b101,
+                    framebuffer_depth_samples_count: 0b101,
+                    framebuffer_stencil_samples_count: 0b101,
                 },
                 private_caps: Capabilities {
                     heterogeneous_resource_heaps,
