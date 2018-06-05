9d0f11f58693967b2db3a92baa7c388bd6b22778
msiglreith <m.siglreith@gmail.com>
2018-06-05 14:49:52 +0200

    dx12: Implement CPU descriptor heap allocation

--- src/backend/dx12/src/descriptors_cpu.rs
+++ src/backend/dx12/src/descriptors_cpu.rs
@@ -1,4 +1,7 @@
 
+use std::collections::HashSet;
+use std::ptr;
+use winapi::Interface;
 use winapi::um::d3d12;
 use wio::com::ComPtr;
 
@@ -17,7 +20,32 @@ impl HeapLinear {
         ty: d3d12::D3D12_DESCRIPTOR_HEAP_TYPE,
         size: usize,
     ) -> Self {
-        unimplemented!()
+        let desc = d3d12::D3D12_DESCRIPTOR_HEAP_DESC {
+            Type: ty,
+            NumDescriptors: size as u32,
+            Flags: d3d12::D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+            NodeMask: 0,
+        };
+
+        let mut heap: *mut d3d12::ID3D12DescriptorHeap = ptr::null_mut();
+        let handle_size = unsafe {
+            device.CreateDescriptorHeap(
+                &desc,
+                &d3d12::ID3D12DescriptorHeap::uuidof(),
+                &mut heap as *mut *mut _ as *mut *mut _,
+            );
+            device.GetDescriptorHandleIncrementSize(ty) as usize
+        };
+
+        let start = unsafe { (*heap).GetCPUDescriptorHandleForHeapStart() };
+
+        HeapLinear {
+            handle_size,
+            num: 0,
+            size,
+            start,
+            raw: unsafe { ComPtr::from_raw(heap) },
+        }
     }
 
     pub fn alloc_handle(&mut self) -> d3d12::D3D12_CPU_DESCRIPTOR_HANDLE {
@@ -32,7 +60,7 @@ impl HeapLinear {
     }
 
     pub fn full(&self) -> bool {
-        self.num < self.size
+        self.num >= self.size
     }
 
     pub fn clear(&mut self) {
@@ -41,7 +69,7 @@ impl HeapLinear {
 }
 
 // Fixed-size (64) free-list allocator for CPU descriptors.
-pub struct Heap {
+struct Heap {
     occupancy: u64,
     handle_size: usize,
     start: d3d12::D3D12_CPU_DESCRIPTOR_HANDLE,
@@ -50,7 +78,30 @@ pub struct Heap {
 
 impl Heap {
     pub fn new(device: &ComPtr<d3d12::ID3D12Device>, ty: d3d12::D3D12_DESCRIPTOR_HEAP_TYPE) -> Self {
-        unimplemented!()
+        let desc = d3d12::D3D12_DESCRIPTOR_HEAP_DESC {
+            Type: ty,
+            NumDescriptors: 64,
+            Flags: d3d12::D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+            NodeMask: 0,
+        };
+
+        let mut heap: *mut d3d12::ID3D12DescriptorHeap = ptr::null_mut();
+        let handle_size = unsafe {
+            device.CreateDescriptorHeap(
+                &desc,
+                &d3d12::ID3D12DescriptorHeap::uuidof(),
+                &mut heap as *mut *mut _ as *mut *mut _,
+            );
+            device.GetDescriptorHandleIncrementSize(ty) as usize
+        };
+        let start = unsafe { (*heap).GetCPUDescriptorHandleForHeapStart() };
+
+        Heap {
+            handle_size,
+            occupancy: 0,
+            start,
+            raw: unsafe { ComPtr::from_raw(heap) },
+        }
     }
 
     pub fn alloc_handle(&mut self) -> d3d12::D3D12_CPU_DESCRIPTOR_HANDLE {
@@ -74,15 +125,41 @@ pub struct DescriptorCpuPool {
     device: ComPtr<d3d12::ID3D12Device>,
     ty: d3d12::D3D12_DESCRIPTOR_HEAP_TYPE,
     heaps: Vec<Heap>,
-    handle_size: usize,
+    free_list: HashSet<usize>,
 }
 
 impl DescriptorCpuPool {
     pub fn new(device: &ComPtr<d3d12::ID3D12Device>, ty: d3d12::D3D12_DESCRIPTOR_HEAP_TYPE) -> Self {
-        unimplemented!()
+        DescriptorCpuPool {
+            device: device.clone(),
+            ty,
+            heaps: Vec::new(),
+            free_list: HashSet::new(),
+        }
     }
 
     pub fn alloc_handle(&mut self) -> d3d12::D3D12_CPU_DESCRIPTOR_HANDLE {
-        unimplemented!()
+        let heap_id = self
+            .free_list
+            .iter()
+            .cloned()
+            .next()
+            .unwrap_or_else(|| {
+                // Allocate a new heap
+                let id = self.heaps.len();
+                self.heaps.push(Heap::new(&self.device, self.ty));
+                self.free_list.insert(id);
+                id
+            });
+
+        let heap = &mut self.heaps[heap_id];
+        let handle = heap.alloc_handle();
+        if heap.full() {
+            self.free_list.remove(&heap_id);
+        }
+
+        handle
     }
+
+    // TODO: free handles
 }
