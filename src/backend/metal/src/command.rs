6b9af63950138f42869083e5c552dcc7cadca772
Dzmitry Malyshau <kvarkus@gmail.com>
2018-09-12 15:01:38 -0400

    [mtl] allow binding of non-written buffers

--- src/backend/metal/src/command.rs
+++ src/backend/metal/src/command.rs
@@ -588,8 +588,11 @@ impl StageResources {
                     pool_offsets.textures += 1;
                 }
                 if layout.content.contains(native::DescriptorContent::BUFFER) {
-                    let (buffer, offset) = data.buffers[pool_offsets.buffers as usize].unwrap();
-                    self.buffers[res_offset.buffers as usize] = Some(buffer);
+                    let (buffer, offset) = match data.buffers[pool_offsets.buffers as usize] {
+                        Some((buffer, offset)) => (Some(buffer), offset),
+                        None => (None, 0),
+                    };
+                    self.buffers[res_offset.buffers as usize] = buffer;
                     self.buffer_offsets[res_offset.buffers as usize] = offset;
                     res_offset.buffers += 1;
                     pool_offsets.buffers += 1;
