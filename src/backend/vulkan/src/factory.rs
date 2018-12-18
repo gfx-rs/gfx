a77f6f028a7d6c4271b50be74bb3b3bef7783168
Dzmitry Malyshau <dmalyshau@mozilla.com>
2018-12-17 21:55:00 -0500

    [gl] don't use temporary manager for resource creation

--- src/backend/vulkan/src/factory.rs
+++ src/backend/vulkan/src/factory.rs
@@ -164,7 +164,7 @@ impl Factory {
             bind: Bind::RENDER_TARGET,
             usage: Usage::Data,
         };
-        let tex = self.frame_handles.make_texture(raw_tex, tex_desc);
+        let tex = self.share.handles.lock().unwrap().make_texture(raw_tex, tex_desc);
         let view_desc = t::RenderDesc {
             channel: format.1,
             level: 0,
@@ -408,7 +408,7 @@ impl core::Factory<R> for Factory {
                     pixel: ps.shader,
                 }
             },
-            core::ShaderSet::Tessellated(..) => unimplemented!(),
+            core::ShaderSet::Tessellated(..) |
             core::ShaderSet::TessellatedGeometry(..) => unimplemented!(),
         };
 
