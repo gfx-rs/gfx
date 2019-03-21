eedd07b554b747c5f5246266b890ac6d95283e10
Joshua Groves <josh@joshgroves.com>
2019-03-20 23:53:28 -0600

    gl: Address review feedback

--- examples/quad/main.rs
+++ examples/quad/main.rs
@@ -33,8 +33,8 @@ extern crate console_error_panic_hook;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 #[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
 pub fn wasm_main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
     main();
