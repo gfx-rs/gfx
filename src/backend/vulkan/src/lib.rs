abe25695528bf447e156592a9fd174ab9b94ea9d
Dzmitry Malyshau <dmalyshau@mozilla.com>
2020-02-12 17:32:54 -0800

    [vk] expect -1 returned by acquire_next_image

--- src/backend/vulkan/src/lib.rs
+++ src/backend/vulkan/src/lib.rs
@@ -708,6 +708,7 @@ impl adapter::PhysicalDevice<Backend> for PhysicalDevice {
                 requested_features,
                 self.instance.clone(),
             )),
+            vendor_id: self.properties.vendor_id,
         };
 
         let device_arc = device.raw.clone();
@@ -1402,6 +1403,7 @@ impl queue::CommandQueue<Backend> for CommandQueue {
 #[derive(Debug)]
 pub struct Device {
     raw: Arc<RawDevice>,
+    vendor_id: u32,
 }
 
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
