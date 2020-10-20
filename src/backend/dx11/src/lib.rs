a2645e079b0d61729a9a541efe9e937bbea04ca8
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-10-20 15:10:11 -0400

    [dx11] Fix descriptor writes involving descriptor arrays

--- src/backend/dx11/src/lib.rs
+++ src/backend/dx11/src/lib.rs
@@ -3308,15 +3308,15 @@ impl<T> MultiStageData<RegisterData<T>> {
 }
 
 impl MultiStageData<RegisterData<DescriptorIndex>> {
-    fn add_content(&mut self, content: DescriptorContent, stages: pso::ShaderStageFlags) {
+    fn add_content_many(&mut self, content: DescriptorContent, stages: pso::ShaderStageFlags, count: DescriptorIndex) {
         if stages.contains(pso::ShaderStageFlags::VERTEX) {
-            self.vs.add_content(content);
+            self.vs.add_content_many(content, count);
         }
         if stages.contains(pso::ShaderStageFlags::FRAGMENT) {
-            self.ps.add_content(content);
+            self.ps.add_content_many(content, count);
         }
         if stages.contains(pso::ShaderStageFlags::COMPUTE) {
-            self.cs.add_content(content);
+            self.cs.add_content_many(content, count);
         }
     }
 
@@ -3444,7 +3444,7 @@ impl DescriptorSetInfo {
             if binding.binding == binding_index {
                 return (content, res_offsets.map(|offset| *offset as ResourceIndex));
             }
-            res_offsets.add_content(content);
+            res_offsets.add_content_many(content, 1);
         }
         panic!("Unable to find binding {:?}", binding_index);
     }
